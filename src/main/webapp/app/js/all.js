ac533acc7c30bb87d997cfb6d4cb9cd53c0e492d
Davin Kevin <davin.kevin@gmail.com>
2014-06-09 00:24:07 +0200

    Raccourcis pour le déplacement dans l'application

--- src/main/webapp/app/js/all.js
+++ src/main/webapp/app/js/all.js
@@ -589,55 +589,170 @@ angular.module('podcastApp', [
     'angular-loading-bar',
     'ngTagsInput'
 ])
-    .config(['$routeProvider',
-        function($routeProvider) {
+    .config(function($routeProvider) {
             $routeProvider.
                 when('/podcasts', {
                     templateUrl: 'html/podcasts-list.html',
-                    controller: 'PodcastsListCtrl'
+                    controller: 'PodcastsListCtrl',
+                    hotkeys: [
+                        ['h', 'Goto Home', function (event) {
+                            event.preventDefault();
+                            window.location.href = '#/items';
+                        }, false],
+                        ['s', 'Goto Search', function (event) {
+                            event.preventDefault();
+                            window.location.href = '#/item/search';
+                        }, false],
+                        ['p', 'Goto Podcast List', function (event) {
+                            event.preventDefault();
+                            window.location.href = '#/podcasts';
+                        }, false],
+                        ['d', 'Goto Download List', function (event) {
+                            event.preventDefault();
+                            window.location.href = '#/download';
+                        }, false]
+                    ]
                 }).
                 when('/podcast/add', {
                     templateUrl: 'html/podcast-add.html',
-                    controller: 'PodcastAddCtrl'
+                    controller: 'PodcastAddCtrl',
+                    hotkeys: [
+                        ['h', 'Goto Home', function (event) {
+                            event.preventDefault();
+                            window.location.href = '#/items';
+                        }, false],
+                        ['s', 'Goto Search', function (event) {
+                            event.preventDefault();
+                            window.location.href = '#/item/search';
+                        }, false],
+                        ['p', 'Goto Podcast List', function (event) {
+                            event.preventDefault();
+                            window.location.href = '#/podcasts';
+                        }, false],
+                        ['d', 'Goto Download List', function (event) {
+                            event.preventDefault();
+                            window.location.href = '#/download';
+                        }, false]
+                    ]
                 }).
                 when('/podcast/:podcastId', {
                     templateUrl: 'html/podcast-detail.html',
                     controller: 'PodcastDetailCtrl',
                     hotkeys: [
-                        ['r', 'Refresh', 'refreshItems()'],
-                        ['f', 'Force Refresh', 'refresh()'],
-                        ['l', 'List of Items', 'tabs[0].active = true'],
-                        ['m', 'Modification of Podcast', 'tabs[1].active = true']
+                        ['r', 'Refresh', 'refreshItems()', false],
+                        ['f', 'Force Refresh', 'refresh()', false],
+                        ['l', 'List of Items', 'tabs[0].active = true', false],
+                        ['m', 'Modification of Podcast', 'tabs[1].active = true', false],
+                        ['h', 'Goto Home', function (event) {
+                            event.preventDefault();
+                            window.location.href = '#/items';
+                        }, false],
+                        ['s', 'Goto Search', function (event) {
+                            event.preventDefault();
+                            window.location.href = '#/item/search';
+                        }, false],
+                        ['p', 'Goto Podcast List', function (event) {
+                            event.preventDefault();
+                            window.location.href = '#/podcasts';
+                        }, false],
+                        ['d', 'Goto Download List', function (event) {
+                            event.preventDefault();
+                            window.location.href = '#/download';
+                        }, false]
                     ]
                 }).
                 when('/items', {
                     templateUrl: 'html/items-list.html',
                     controller: 'ItemsListCtrl',
                     hotkeys: [
-                        ['right', 'Next page', 'currentPage = currentPage+1; changePage();'],
-                        ['left', 'Previous page', 'currentPage = currentPage-1; changePage();']
+                        ['right', 'Next page', 'currentPage = currentPage+1; changePage();', false],
+                        ['left', 'Previous page', 'currentPage = currentPage-1; changePage();', false],
+                        ['h', 'Goto Home', function (event) {
+                            event.preventDefault();
+                            window.location.href = '#/items';
+                        }, false],
+                        ['s', 'Goto Search', function (event) {
+                            event.preventDefault();
+                            window.location.href = '#/item/search';
+                        }, false],
+                        ['p', 'Goto Podcast List', function (event) {
+                            event.preventDefault();
+                            window.location.href = '#/podcasts';
+                        }, false]                      ,
+                        ['d', 'Goto Download List', function (event) {
+                            event.preventDefault();
+                            window.location.href = '#/download';
+                        }, false]
                     ]
                 }).
                 when('/item/search', {
                     templateUrl: 'html/items-search.html',
                     controller: 'ItemsSearchCtrl',
                     hotkeys: [
-                        ['right', 'Next page', 'currentPage = currentPage+1; changePage();'],
-                        ['left', 'Previous page', 'currentPage = currentPage-1; changePage();']
+                        ['right', 'Next page', 'currentPage = currentPage+1; changePage();', false],
+                        ['left', 'Previous page', 'currentPage = currentPage-1; changePage();', false],
+                        ['h', 'Goto Home', function (event) {
+                            event.preventDefault();
+                            window.location.href = '#/items';
+                        }, false],
+                        ['s', 'Goto Search', function (event) {
+                            event.preventDefault();
+                            window.location.href = '#/item/search';
+                        }, false],
+                        ['p', 'Goto Podcast List', function (event) {
+                            event.preventDefault();
+                            window.location.href = '#/podcasts';
+                        }, false],
+                        ['d', 'Goto Download List', function (event) {
+                            event.preventDefault();
+                            window.location.href = '#/download';
+                        }, false]
                     ]
                 }).
                 when('/item/:itemId', {
                     templateUrl: 'html/item-detail.html',
-                    controller: 'ItemDetailCtrl'
+                    controller: 'ItemDetailCtrl',
+                    hotkeys: [
+                        ['h', 'Goto Home', function (event) {
+                            event.preventDefault();
+                            window.location.href = '#/items';
+                        }, false],
+                        ['s', 'Goto Search', function (event) {
+                            event.preventDefault();
+                            window.location.href = '#/item/search';
+                        }, false],
+                        ['p', 'Goto Podcast List', function (event) {
+                            event.preventDefault();
+                            window.location.href = '#/podcasts';
+                        }, false],
+                        ['d', 'Goto Download List', function (event) {
+                            event.preventDefault();
+                            window.location.href = '#/download';
+                        }, false],
+                    ]
                 }).
                 when('/download', {
                     templateUrl: 'html/download.html',
-                    controller: 'DownloadCtrl'
+                    controller: 'DownloadCtrl',
+                    hotkeys: [
+                        ['h', 'Goto Home', function (event) {
+                            event.preventDefault();
+                            window.location.href = '#/items';
+                        }, false],
+                        ['s', 'Goto Search', function (event) {
+                            event.preventDefault();
+                            window.location.href = '#/item/search';
+                        }, false],
+                        ['p', 'Goto Podcast List', function (event) {
+                            event.preventDefault();
+                            window.location.href = '#/podcasts';
+                        }, false]
+                    ]
                 }).
                 otherwise({
                     redirectTo: '/items'
                 });
-        }])
+        })
     .config(['cfpLoadingBarProvider', function (cfpLoadingBarProvider) {
         cfpLoadingBarProvider.includeSpinner = false;
     }])
