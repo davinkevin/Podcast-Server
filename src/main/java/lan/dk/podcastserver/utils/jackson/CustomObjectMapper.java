d7a2bdabe259140f44d9e342f28ea7203a9e4e6a
Davin Kevin <davin.kevin@gmail.com>
2014-09-27 23:44:20 +0200

    Fix problem with lazy initialisation

--- src/main/java/lan/dk/podcastserver/utils/jackson/CustomObjectMapper.java
+++ src/main/java/lan/dk/podcastserver/utils/jackson/CustomObjectMapper.java
@@ -2,15 +2,22 @@ package lan.dk.podcastserver.utils.jackson;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.hibernate4.Hibernate4Module;
 import com.fasterxml.jackson.datatype.jsr310.JSR310Module;
 
 public class CustomObjectMapper extends ObjectMapper {
 
     public CustomObjectMapper() {
-        //registerModule(new Hibernate4Module());
+        registerModule(hibernate4Module());
         registerModule(new JSR310Module());
+
         configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, true);
         configure(SerializationFeature.WRITE_DATE_TIMESTAMPS_AS_NANOSECONDS, false);
+    }
+
+    private Hibernate4Module hibernate4Module() {
+        return new Hibernate4Module()
+                .configure(Hibernate4Module.Feature.USE_TRANSIENT_ANNOTATION, false);
     }
 
 }
