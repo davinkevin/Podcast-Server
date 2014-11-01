bd142ace6cf014cadb44a0bc39a354c2bf20e161
Davin Kevin <davin.kevin@gmail.com>
2014-11-01 22:28:48 +0100

    feat(item): improve hashset calculation
    Adding the pubdate in instant

--- src/main/java/lan/dk/podcastserver/entity/Item.java
+++ src/main/java/lan/dk/podcastserver/entity/Item.java
@@ -268,7 +268,7 @@ public class Item implements Serializable {
         return new HashCodeBuilder(17, 37)
                 .append(title)
                 .append(FilenameUtils.getName(url))
-                .append(pubdate.getNano())
+                .append(pubdate.toInstant())
                 .toHashCode();
     }
 
