a39fca2d4241ceb7018d2633ed7148bc30fe5662
Davin Kevin <davin.kevin@gmail.com>
2015-02-02 00:14:09 +0100

    fix(updater): Timeout on connection when reading for digest

--- src/main/java/lan/dk/podcastserver/utils/SignatureUtils.java
+++ src/main/java/lan/dk/podcastserver/utils/SignatureUtils.java
@@ -6,19 +6,28 @@ import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.net.URL;
+import java.net.URLConnection;
 
 /**
  * Created by kevin on 24/01/15.
  */
 public class SignatureUtils {
 
+    private static final int CONNECT_TIMEOUT = 5000;
+    private static final int READ_TIMEOUT = 5000;
 
     private static final Logger logger = LoggerFactory.getLogger(SignatureUtils.class);
-    
+
     public static String generateSignatureFromURL(String urlAsString) {
         try {
             URL url = new URL(urlAsString);
-            return DigestUtils.md5Hex(url.openStream());
+
+            URLConnection conn = url.openConnection();
+            
+            conn.setConnectTimeout(CONNECT_TIMEOUT);
+            conn.setReadTimeout(READ_TIMEOUT);
+            
+            return DigestUtils.md5Hex(conn.getInputStream());
         } catch (IOException e) {
             logger.error("Error during signature of podcast at url {}", urlAsString, e);
         }
