e00cc1d8565e823e6456fee07bf225ab58aba392
Davin Kevin <davin.kevin@gmail.com>
2015-04-03 06:34:33 +0200

    refactor(update.controller): remove all produce annotation

--- src/main/java/lan/dk/podcastserver/controller/task/UpdatePodcastController.java
+++ src/main/java/lan/dk/podcastserver/controller/task/UpdatePodcastController.java
@@ -12,38 +12,35 @@ import javax.annotation.Resource;
 @RequestMapping("/api/task/updateManager")
 public class UpdatePodcastController {
 
-    @Resource
-    UpdatePodcastBusiness updatePodcastBusiness;
+    @Resource UpdatePodcastBusiness updatePodcastBusiness;
+    @Resource ItemDownloadManager IDM;
 
-    @Resource
-    ItemDownloadManager IDM;
-
-    @RequestMapping(value = "/updatePodcast", method = RequestMethod.GET, produces = "application/json")
+    @RequestMapping(value = "/updatePodcast", method = RequestMethod.GET)
     @ResponseStatus(value = HttpStatus.NO_CONTENT)
     private void updatePodcast () {
         updatePodcastBusiness.updateAsyncPodcast();
     }
 
-    @RequestMapping(value = "/updatePodcast", method = RequestMethod.POST, produces = "application/json")
+    @RequestMapping(value = "/updatePodcast", method = RequestMethod.POST)
     @ResponseStatus(value = HttpStatus.NO_CONTENT)
     private void updatePodcast (@RequestBody int id) {
         updatePodcastBusiness.updatePodcast(id);
     }
 
-    @RequestMapping(value = "/updatePodcast/force", method = RequestMethod.POST, produces = "application/json")
+    @RequestMapping(value = "/updatePodcast/force", method = RequestMethod.POST)
     @ResponseStatus(value = HttpStatus.NO_CONTENT)
     private void updatePodcastForced (@RequestBody int id) {
         updatePodcastBusiness.forceUpdatePodcast(id);
     }
 
-    @RequestMapping(value = "/updateAndDownloadPodcast", method = RequestMethod.GET, produces = "application/json")
+    @RequestMapping(value = "/updateAndDownloadPodcast", method = RequestMethod.GET)
     @ResponseStatus(value = HttpStatus.NO_CONTENT)
     private void updateAndDownloadPodcast() {
         updatePodcastBusiness.updateAsyncPodcast();
         IDM.launchDownload();
     }
 
-    @RequestMapping(value = "/deleteOdlItems", method = RequestMethod.GET, produces = "application/json")
+    @RequestMapping(value = "/deleteOdlItems", method = RequestMethod.GET)
     @ResponseStatus(value = HttpStatus.NO_CONTENT)
     private void deleteOldItem() {
         updatePodcastBusiness.deleteOldEpisode();
