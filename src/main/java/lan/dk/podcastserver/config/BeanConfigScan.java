3764280512409ab200569b0c39997eb3b8396f93
Davin Kevin <davin.kevin@gmail.com>
2015-05-10 10:43:43 +0200

    fix(backend.config): remove duplicate annotation about async config

--- src/main/java/lan/dk/podcastserver/config/BeanConfigScan.java
+++ src/main/java/lan/dk/podcastserver/config/BeanConfigScan.java
@@ -3,7 +3,6 @@ package lan.dk.podcastserver.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 /**
@@ -13,7 +12,6 @@ import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 @ComponentScan(basePackages = { "lan.dk.podcastserver.utils",
                                 "lan.dk.podcastserver.service",
                                 "lan.dk.podcastserver.business"})
-@EnableAsync
 public class BeanConfigScan {
 
     @Bean(name="Validator")
