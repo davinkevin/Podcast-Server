3c57ce2760e6932824d60df0b93e64b0e7c55a19
Davin Kevin <davin.kevin@gmail.com>
2015-05-16 15:05:17 +0200

    feat(updater.send): fake implemenation to provide a way to handle this type of podcast in the whole application

---
+++ src/main/java/lan/dk/podcastserver/manager/worker/updater/SendUpdater.java
@@ -0,0 +1,35 @@
+package lan.dk.podcastserver.manager.worker.updater;
+
+import lan.dk.podcastserver.entity.Item;
+import lan.dk.podcastserver.entity.Podcast;
+import org.springframework.stereotype.Component;
+
+import java.util.Set;
+import java.util.function.Predicate;
+
+/**
+ * Created by kevin on 15/05/15 for HackerRank problem
+ */
+@Component("SendUpdater")
+public class SendUpdater extends AbstractUpdater {
+
+    @Override
+    public Set<Item> getItems(Podcast podcast) {
+        return podcast.getItems();
+    }
+
+    @Override
+    public String signatureOf(Podcast podcast) {
+        return "";
+    }
+
+    @Override
+    public Predicate<Item> notIn(Podcast podcast) {
+        return item -> Boolean.FALSE;
+    }
+
+    @Override
+    public Type type() {
+        return new Type("send", "Send");
+    }
+}
