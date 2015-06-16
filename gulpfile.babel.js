14e446f9f77391836897b5eedcc7b0b34e6d12fd
Davin Kevin <davin.kevin@gmail.com>
2015-06-16 21:12:25 +0200

    chore(build): upgrade gulp to es6
    Closed #23

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,27 +1,31 @@
 // Include gulp and Our Plugins
-var gulp = require('gulp'),
-    jshint = require('gulp-jshint'),
-    less = require('gulp-less'),
-    minifyCSS = require('gulp-minify-css'),
-    ngAnnotate = require('gulp-ng-annotate'),
-    concat = require('gulp-concat'),
-    uglify = require('gulp-uglify'),
-    rename = require('gulp-rename'),
-    templateCache = require('gulp-angular-templatecache'),
-    inject = require("gulp-inject"),
-    bowerFiles = require('main-bower-files'),
-    debug = require('gulp-debug'),
-    sourcemaps = require('gulp-sourcemaps'),
-    args = require('yargs').argv,
-    plumber = require('gulp-plumber'),
-    es = require('event-stream'),
-    babel = require('gulp-babel'),
-    wrap = require("gulp-wrap-js"),
-    connect = require('gulp-connect'),
-    proxy = require('proxy-middleware'),
-    urlparser = require('url'),
-    cachebust = require('gulp-cache-bust'),
-    bower = require('gulp-bower');
+
+import gulp from 'gulp';
+import jshint from 'gulp-jshint';
+import less from 'gulp-less';
+import minifyCSS from 'gulp-minify-css';
+import ngAnnotate from 'gulp-ng-annotate';
+import concat from 'gulp-concat';
+import uglify from 'gulp-uglify';
+import rename from 'gulp-rename';
+import templateCache from 'gulp-angular-templatecache';
+import inject from "gulp-inject";
+import bowerFiles from 'main-bower-files';
+import debug from 'gulp-debug';
+import sourcemaps from 'gulp-sourcemaps';
+import yargs from 'yargs';
+import plumber from 'gulp-plumber';
+import es from 'event-stream';
+import babel from 'gulp-babel';
+import wrap from "gulp-wrap-js";
+import connect from 'gulp-connect';
+import proxy from 'proxy-middleware';
+import urlparser from 'url';
+import cachebust from 'gulp-cache-bust';
+import bower from 'gulp-bower';
+
+let args = yargs.argv;
+let env = args.env  || 'prod';
 
 const   staticLocation = 'src/main/resources/static/',
         appLocation = staticLocation + 'app/',
@@ -41,17 +45,26 @@ const   appName = 'podcastserver',
         appCssFileName = appName + '.css',
         appFiles = [cssDestionation + appCssFileName, jsDestination + appJsFileName];
 
-var env = args.env  || 'prod';
+let redirect = (route) => {
+    return {
+        to : function(remoteUrl) {
+            var options = urlparser.parse(remoteUrl);
+            options.route = route;
+            options.preserveHost = true;
+            return proxy(options);
+        }
+    }
+};
 
 // Lint Task
-gulp.task('lint', function() {
-    return gulp.src(angularAppLocation)
-        .pipe(jshint())
-        .pipe(jshint.reporter('default'));
-});
+gulp.task('lint', () =>
+    gulp.src(angularAppLocation)
+      .pipe(jshint())
+      .pipe(jshint.reporter('default'))
+);
 
-gulp.task('js', function() {
-    return es.merge(
+gulp.task('js', () =>
+     es.merge(
             gulp.src(htmlLocation).pipe(plumber()).pipe(templateCache('partial.js',{ standalone : true, module: "ps.partial", root: "html/" })).pipe(concat("partials.js")),
             gulp.src(angularAppLocation)
         )
@@ -65,11 +78,11 @@ gulp.task('js', function() {
         .pipe(rename({suffix : '.min'}))
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest(jsDestination))
-        .pipe(connect.reload());
-});
+        .pipe(connect.reload())
+);
 
-gulp.task('less', function () {
-    return gulp.src(lessMainLocation)
+gulp.task('less', () =>
+    gulp.src(lessMainLocation)
         .pipe(plumber())
         .pipe(less())
         .pipe(concat(appCssFileName))
@@ -77,21 +90,19 @@ gulp.task('less', function () {
         .pipe(minifyCSS({keepBreaks: true}))
         .pipe(rename({suffix : '.min'}))
         .pipe(gulp.dest(cssDestionation))
-        .pipe(connect.reload());
-});
+        .pipe(connect.reload())
+);
 
 // Watch Files For Changes
-gulp.task('watch',['js', 'less', 'inject', 'webserver'], function() {
+gulp.task('watch',['js', 'less', 'inject', 'webserver'], () => {
     gulp.watch([angularAppLocation, htmlLocation], ['js', 'lint']);
     gulp.watch(lessLocation, ['less']);
     gulp.watch(bowerDependency , ['inject']);
 });
 
-gulp.task('bower-install', function() {
-    return bower();
-});
+gulp.task('bower-install', () => bower() );
 
-gulp.task('inject', ['bower-install'], function() {
+gulp.task('inject', ['bower-install'], () => {
 
     var min = (env === 'dev') ? '' : '.min';
 
@@ -106,21 +117,8 @@ gulp.task('inject', ['bower-install'], function() {
         .pipe(gulp.dest(appLocation + 'js/lib/'));
 });
 
-gulp.task('webserver', function() {
-
-    function redirect(route) {
-        return {
-            to : function(remoteUrl) {
-                var options = urlparser.parse(remoteUrl);
-                options.route = route;
-                options.preserveHost = true;
-                return proxy(options);
-            }
-        }
-
-    }
-
-    var port = parseInt(args.port) || 8000;
+gulp.task('webserver', () => {
+    let port = parseInt(args.port) || 8000;
 
     connect.server({
         root: staticLocation,
