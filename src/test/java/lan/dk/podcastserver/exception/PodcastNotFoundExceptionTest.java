9642cf4f1275577241b537c92437d663f4547620
Davin Kevin <davin.kevin@gmail.com>
2015-09-02 05:26:32 +0200

    test(exception): expand testing on Exception

---
+++ src/test/java/lan/dk/podcastserver/exception/PodcastNotFoundExceptionTest.java
@@ -0,0 +1,17 @@
+package lan.dk.podcastserver.exception;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.StrictAssertions.assertThat;
+
+/**
+ * Created by kevin on 17/08/15 for Podcast Server
+ */
+public class PodcastNotFoundExceptionTest {
+
+    @Test
+    public void should_have_coherent_object() {
+        assertThat(new PodcastNotFoundException())
+                .isOfAnyClassIn(PodcastNotFoundException.class);
+    }
+}
