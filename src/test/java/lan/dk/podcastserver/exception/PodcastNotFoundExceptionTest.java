26c613cf84df7380fcf77c49413d2801f2d0077f
Davin Kevin <davin.kevin@gmail.com>
2015-09-26 09:27:24 +0200

    chore(assert) update assertion framework

--- src/test/java/lan/dk/podcastserver/exception/PodcastNotFoundExceptionTest.java
+++ src/test/java/lan/dk/podcastserver/exception/PodcastNotFoundExceptionTest.java
@@ -2,7 +2,7 @@ package lan.dk.podcastserver.exception;
 
 import org.junit.Test;
 
-import static org.assertj.core.api.StrictAssertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * Created by kevin on 17/08/15 for Podcast Server
