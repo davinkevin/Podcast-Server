e9b38e136325bcc8674703e3392353b7175cd824
Davin Kevin <davin.kevin@gmail.com>
2015-10-02 05:53:50 +0200

    test(updater.youtube): expand testing

--- src/test/java/lan/dk/podcastserver/manager/worker/updater/YoutubeUpdaterTest.java
+++ src/test/java/lan/dk/podcastserver/manager/worker/updater/YoutubeUpdaterTest.java
@@ -82,6 +82,34 @@ public class YoutubeUpdaterTest {
         verify(htmlService, only()).connectWithDefault(eq("https://www.youtube.com/user/androiddevelopers"));
     }
 
+    @Test
+    public void should_generate_signature() throws JDOMException, IOException, URISyntaxException {
+        /* Given */
+        Podcast podcast = Podcast.builder()
+                .url("https://www.youtube.com/user/androiddevelopers")
+                .build();
+
+        Connection connection = mock(Connection.class);
+        Document document = mock(Document.class);
+        Elements elements = mock(Elements.class);
+        Element theFirstElement = mock(Element.class);
+        when(htmlService.connectWithDefault(any(String.class))).thenReturn(connection);
+        when(connection.get()).thenReturn(document);
+        when(document.select(anyString())).thenReturn(elements);
+        when(elements.first()).thenReturn(theFirstElement);
+        when(theFirstElement.attr(anyString())).thenReturn("UCVHFbqXqoYvEWM1Ddxl0QDg");
+        when(jdomService.parse(anyString())).then(parseFromFile("/remote/podcast/youtube.androiddevelopers.xml"));
+        when(signatureService.generateMD5Signature(anyString())).thenReturn("Signature");
+
+        /* When */
+        String signature = youtubeUpdater.signatureOf(podcast);
+
+        /* Then */
+        assertThat(signature).isEqualTo("Signature");
+        verify(jdomService, only()).parse(eq("https://www.youtube.com/feeds/videos.xml?channel_id=UCVHFbqXqoYvEWM1Ddxl0QDg"));
+        verify(htmlService, only()).connectWithDefault(eq("https://www.youtube.com/user/androiddevelopers"));
+    }
+
     private Answer<Object> parseFromFile(String file) throws JDOMException, IOException, URISyntaxException {
         return invocationOnMock -> new SAXBuilder().build(Paths.get(RSSUpdaterTest.class.getResource(file).toURI()).toFile());
     }
