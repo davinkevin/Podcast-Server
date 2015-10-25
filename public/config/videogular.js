b8fd99c2a089f550663eb25c7fabc27363805359
Davin Kevin <davin.kevin@gmail.com>
2015-10-25 02:37:11 +0200

    refactor(item.player): refactor item.player module
    Linked to #33

--- public/config/videogular.js
+++ public/config/videogular.js
@@ -1,13 +1,21 @@
 /**
  * Created by kevin on 25/10/2015 for PodcastServer
  */
+
 import angular from 'angular';
+import angularSanitize from 'angular-sanitize';
+import Videogular from 'videogular';
+import VideogularBuffering from 'videogular-buffering';
+import VideogularControls from 'videogular-controls';
+import VideogularPoster from 'videogular-poster';
+import VideogularOverlayPlay from 'videogular-overlay-play';
+import '/jspm_packages/github/2fdevs/bower-videogular-themes-default@1.3.2/videogular.css!';
 
 export default angular.module('ps.config.videogular', [
     'ngSanitize',
-    'com.2fdevs.videogular',
-    'com.2fdevs.videogular.plugins.poster',
-    'com.2fdevs.videogular.plugins.controls',
-    'com.2fdevs.videogular.plugins.overlayplay',
-    'com.2fdevs.videogular.plugins.buffering',
+    Videogular,
+    VideogularPoster,
+    VideogularControls,
+    VideogularOverlayPlay,
+    VideogularBuffering
 ]);
--- public/item/player/item.player.js
+++ public/item/player/item.player.js
@@ -1,7 +1,7 @@
 import angular from 'angular';
 import AppVideogularConfig from 'config/videogular';
 import AppRouteConfig from 'config/route.config';
-import DeviceDetectionService from 'component/device-detection/device-detection';
+import DeviceDetectionService from 'common/component/device-detection/device-detection';
 import ItemPlayerController from './item.player.controller';
 
 export default angular.module('ps.item.player', [
