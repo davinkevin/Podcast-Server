8bb713c411fbc97bf7ba3cca6d0be306242662e4
Davin Kevin <davin.kevin@gmail.com>
2015-10-26 06:00:28 +0100

    refactor(authorize-notification): refactor for jspm

---
+++ public/common/component/authorize-notification/authorize-notification.component.js
@@ -0,0 +1,35 @@
+/**
+    * Created by kevin on 25/10/2015 for PodcastServer
+    */
+
+import template from './authorize-notification.html!text';
+
+export default class AuthorizeNotificationComponent {
+    constructor($window, $notification) {
+        this.$window = $window;
+        this.$notification = $notification;
+        this.state = this.hasToBeShown();
+    }
+
+    manuallyactivate() {
+        this.$notification
+            .requestPermission()
+            .then(() => { this.state = this.hasToBeShown();});
+    }
+
+    hasToBeShown() {
+        return (('Notification' in this.$window) && this.$window.Notification.permission != 'granted');
+    }
+    
+    static component() {
+        return {
+            replace : true,
+            restrict : 'E',
+            scope : true,
+            template : template,
+            controllerAs : 'an',
+            controller : 'authorizeNotificationController'
+        }
+    }
+}
+
--- public/common/component/authorize-notification/authorize-notification.js
+++ public/common/component/authorize-notification/authorize-notification.js
@@ -1,33 +1,9 @@
+import angular from 'angular';
+import AngularNotification from 'config/angularNotification';
+import AuthoriszeNotificationComponent from './authorize-notification.component';
 
-class authorizeNotificationDirective {
-    constructor() {
-        this.replace = true;
-        this.restrict = 'E';
-        this.scope = true;
-        this.templateUrl = 'common/component/authorize-notification/authorize-notification.html';
-        this.controllerAs = 'an';
-        this.controller = 'authorizeNotificationController';
-    }
-}
-
-class authorizeNotificationController {
-    constructor($window, $notification) {
-        this.$window = $window;
-        this.$notification = $notification;
-        this.state = this.hasToBeShown();
-    }
-
-    manuallyactivate() {
-        this.$notification
-            .requestPermission()
-            .then(() => { this.state = this.hasToBeShown();});
-    }
-
-    hasToBeShown() {
-        return (('Notification' in this.$window) && this.$window.Notification.permission != 'granted');
-    }
-}
-
-angular.module('ps.common.component.authorize-notification', ['notification'])
-    .directive('authorizeNotification', () => new authorizeNotificationDirective())
-    .controller('authorizeNotificationController', authorizeNotificationController);
+export default angular.module('ps.common.component.authorize-notification', [
+    AngularNotification.name
+])
+    .directive('authorizeNotification', AuthoriszeNotificationComponent.component)
+    .controller('authorizeNotificationController', AuthoriszeNotificationComponent);
