2a0142efd573e2620bb4b7e7a96933409cd0476c
Davin Kevin <davin.kevin@gmail.com>
2015-10-26 05:59:55 +0100

    refactor(serve): switch back to gulp-connect for WS functionnality

--- gulp/tasks/serve.js
+++ gulp/tasks/serve.js
@@ -1,33 +1,45 @@
 import gulp from 'gulp';
 import util from 'gulp-util';
 import browserSync from 'browser-sync';
+import proxy from 'proxy-middleware';
+import connect from 'gulp-connect';
+import urlParser from 'url';
 import runSequence from 'run-sequence';
-import modRewrite  from 'connect-modrewrite';
 import paths from '../paths';
 
-function startBrowserSync(directoryBase, files) {
-  files = files === undefined ? 'default' : files;
+let redirect = (route) => {
+  return {
+    to : function(remoteUrl) {
+      let options = urlParser.parse(remoteUrl);
+      options.route = route;
+      options.preserveHost = true;
+      return proxy(options);
+    }
+  }
+};
 
-  browserSync({
-    files: files,
-    open: true,
+function startServer(directoryBase) {
+
+  connect.server({
+    root: directoryBase,
     port: 8000,
-    notify: true,
-    server: {
-      baseDir: directoryBase,
-      middleware: [
-        modRewrite(['!\\.\\w+$ /index.html [L]']) // require for HTML5 mode
-      ]
-    },
-    browser: 'default'
+    livereload: true,
+    fallback: `${paths.srcDir}/index.html`,
+    middleware: function() {
+      return [
+        redirect('/api').to('http://localhost:8080/api'),
+        redirect('/ws').to('http://localhost:8080/ws')
+      ];
+    }
   });
+
 }
 
 gulp.task('serve', ['less'], () => {
 
-  startBrowserSync([paths.srcDir, './' ]);
+  startServer([paths.srcDir, paths.root ]);
 
-  gulp.watch(`${paths.glob.less}`, ['less', browserSync.reload ]);
-  gulp.watch(`${paths.glob.js}`, ['lint-js', browserSync.reload ]);
+  gulp.watch(`${paths.glob.less}`, ['less', connect.reload ]);
+  gulp.watch(`${paths.glob.js}`, ['lint-js', connect.reload ]);
 
 });
