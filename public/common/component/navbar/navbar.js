1a09dde5bc00d25178c0324b73fd86018b588036
Davin Kevin <davin.kevin@gmail.com>
2015-10-26 06:00:46 +0100

    refactor(navbar): refactor for jspm

--- public/common/component/navbar/navbar.js
+++ public/common/component/navbar/navbar.js
@@ -1,26 +1,6 @@
+import angular from 'angular';
+import NavbarComponent from './navbar.component';
 
-class navBarController {
-    constructor() {
-        this.navCollapsed = true;
-    }
-}
-
-class navbarDirective {
-    constructor() {
-        this.transclude = true;
-        this.replace = true;
-        this.restrict = 'E';
-        this.scope = true;
-        this.templateUrl = 'common/component/navbar/navbar.html';
-        this.controller = 'navBarController';
-        this.controllerAs = 'navbar';
-    }
-
-    link(scope, element) {
-        element.removeClass('hidden');
-    }
-}
-
-angular.module('ps.common.component.navbar', [])
-    .directive('navbar', () => new navbarDirective())
-    .controller('navBarController', navBarController);
+export default angular.module('ps.common.component.navbar', [])
+    .directive('navbar', NavbarComponent.component)
+    .controller('navBarController', NavbarComponent);
