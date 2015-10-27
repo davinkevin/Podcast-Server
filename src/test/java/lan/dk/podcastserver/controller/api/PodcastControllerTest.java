5bc269619029561707be9be17e1a6c162cf24218
Davin Kevin <davin.kevin@gmail.com>
2015-10-27 05:24:30 +0100

    test(podcast): expand testing

--- src/test/java/lan/dk/podcastserver/controller/api/PodcastControllerTest.java
+++ src/test/java/lan/dk/podcastserver/controller/api/PodcastControllerTest.java
@@ -4,17 +4,18 @@ import lan.dk.podcastserver.business.PodcastBusiness;
 import lan.dk.podcastserver.business.find.FindPodcastBusiness;
 import lan.dk.podcastserver.business.stats.StatsBusiness;
 import lan.dk.podcastserver.entity.Podcast;
+import lan.dk.podcastserver.entity.PodcastAssert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
-import static lan.dk.podcastserver.entity.PodcastAssert.assertThat;
-import static org.mockito.Matchers.*;
-import static org.mockito.Mockito.only;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Matchers.same;
+import static org.mockito.Mockito.*;
 
 /**
  * Created by kevin on 30/09/15 for Podcast Server
@@ -37,7 +38,7 @@ public class PodcastControllerTest {
         Podcast createdPodcast = podcastController.create(podcast);
 
         /* Then */
-        assertThat(createdPodcast).isEqualTo(podcast);
+        PodcastAssert.assertThat(createdPodcast).isEqualTo(podcast);
         verify(podcastBusiness, only()).create(eq(podcast));
     }
 
@@ -51,7 +52,7 @@ public class PodcastControllerTest {
         Podcast podcastById = podcastController.findById(1);
 
         /* Then */
-        assertThat(podcastById).isSameAs(podcast);
+        PodcastAssert.assertThat(podcastById).isSameAs(podcast);
         verify(podcastBusiness, only()).findOne(eq(1));
     }
 
@@ -64,7 +65,7 @@ public class PodcastControllerTest {
         Podcast podcastUpdated = podcastController.update(podcast, 1);
 
         /* Then */
-        assertThat(podcastUpdated)
+        PodcastAssert.assertThat(podcastUpdated)
                 .isInstanceOf(Podcast.class)
                 .hasId(1);
         verify(podcastBusiness, only()).reatachAndSave(same(podcast));
@@ -80,10 +81,21 @@ public class PodcastControllerTest {
         Podcast podcastUpdated = podcastController.patchUpdate(podcast, 1);
 
         /* Then */
-        assertThat(podcastUpdated)
+        PodcastAssert.assertThat(podcastUpdated)
                 .isInstanceOf(Podcast.class)
                 .hasId(1);
         verify(podcastBusiness, only()).patchUpdate(same(podcast));
     }
 
+    @Test
+    public void should_delete() {
+        /* Given */
+        Integer id = 123;
+
+        /* When */
+        podcastController.delete(id);
+
+        /* Then */
+        verify(podcastBusiness, only()).delete(eq(id));
+    }
 }
