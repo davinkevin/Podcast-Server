8b64d33f132dc88e3dcf2fed6a813a70e2fb48e9
Davin Kevin <davin.kevin@gmail.com>
2015-11-29 06:47:58 +0100

    chore(analysis): modify exclusion for eslintrc

--- www/app/common/mixins.js
+++ www/app/common/mixins.js
@@ -7,7 +7,7 @@ import _ from 'lodash';
 _.mixin({
     // Update in place, does not preserve order
     updateinplace : function(localArray, remoteArray, comparisonFunction = (inArray, elem) => inArray.indexOf(elem), withOrder = false) {
-
+        /*eslint-disable */
         // Remove from localArray what is not in the remote array :
         _.forEachRight(localArray.slice(), (elem, key) => {
             (comparisonFunction(remoteArray, elem) === -1) && localArray.splice(key, 1);
@@ -17,7 +17,7 @@ _.mixin({
         _.forEach(remoteArray, (elem) => {
             (comparisonFunction(localArray, elem) === -1) && localArray.push(elem);
         });
-
+        /*eslint-enable */
         if (withOrder) {
             _.forEach(remoteArray, (elem, key) => {
                 var elementToMove = localArray.splice(comparisonFunction(localArray, elem), 1)[0];
