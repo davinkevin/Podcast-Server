26fa4d120fc589acc326e440ab762ba5142131e1
Davin Kevin <davin.kevin@gmail.com>
2015-12-27 11:22:48 +0100

    fix(updater.beinsports): modification to support new organisation since December 17 2015.
    Linked to #50

--- src/main/java/lan/dk/podcastserver/manager/worker/updater/BeInSportsUpdater.java
+++ src/main/java/lan/dk/podcastserver/manager/worker/updater/BeInSportsUpdater.java
@@ -1,12 +1,11 @@
 package lan.dk.podcastserver.manager.worker.updater;
 
+import com.google.common.collect.Sets;
 import lan.dk.podcastserver.entity.Item;
 import lan.dk.podcastserver.entity.Podcast;
 import lan.dk.podcastserver.service.HtmlService;
 import lan.dk.podcastserver.service.ImageService;
-import lan.dk.podcastserver.service.JdomService;
 import org.apache.commons.lang3.StringUtils;
-import org.jsoup.Connection;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
@@ -14,12 +13,18 @@ import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.ZonedDateTime;
-import java.util.HashSet;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 import java.util.Set;
 import java.util.function.Predicate;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import static java.util.Objects.isNull;
+import static java.util.stream.Collectors.toSet;
 
 /**
  * Created by kevin on 22/02/2014.
@@ -36,69 +41,63 @@ public class BeInSportsUpdater extends AbstractUpdater {
     private static final Pattern STREAM_720_URL_EXTRACTOR_PATTERN1 = Pattern.compile(String.format(ATTRIBUTE_EXTRACTOR_FROM_JAVASCRIPT_VALUE, "url"));
     private static final Pattern POSTER_URL_EXTRACTOR_PATTERN = Pattern.compile(String.format(ATTRIBUTE_EXTRACTOR_FROM_JAVASCRIPT_VALUE, "poster_url"));
     private static final String beInSportsDomain = "http://www.beinsports.com/%s";
+    /* December 26, 2015 13:53 */
+    private static final DateTimeFormatter BEINSPORTS_PARSER = DateTimeFormatter.ofPattern("MMMM d, y HH:mm", Locale.ENGLISH);
 
-    @Resource JdomService jdomService;
     @Resource HtmlService htmlService;
     @Resource ImageService imageService;
 
     public Set<Item> getItems(Podcast podcast) {
         Document page;
-        Set<Item> itemSet = new HashSet<>();
-        String listingUrl = podcast.getUrl();
         try {
-
-            Connection.Response response = htmlService.connectWithDefault(listingUrl).execute();
-            page = response.parse();
+            page = htmlService.connectWithDefault(podcast.getUrl()).execute().parse();
         } catch (IOException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
             logger.error("IOException :", e);
-            return itemSet;
+            return Sets.newHashSet();
         }
 
-        for(Element article : page.select("article")) {
-            Item item = new Item()
-                    .setTitle(article.select("h3").first().text())
-                    .setDescription(article.select("h3").first().text())
-                    .setPubdate(getPubDateFromDescription(article.select("time").first().attr("datetime")));
-            
-            item = getDetailsByJavascript(item, String.format(beInSportsDomain, article.select("a").first().attr("data-url")));
-            itemSet.add(item);
-        }
-
-        return itemSet;
+        return page.select("article")
+                .stream()
+                .map(this::getItem)
+                .collect(toSet());
     }
 
-    private ZonedDateTime getPubDateFromDescription(String dateString) {
-        /* 2015-09-04T04:26:18+00:00 */
-        return ZonedDateTime.parse(dateString);
+    private ZonedDateTime getPubDateFromDescription(Element article) {
+        Element time = article.select("time").first();
+        /* December 26, 2015 13:53 */
+        return isNull(time)
+                ? ZonedDateTime.now()
+                : ZonedDateTime.of(LocalDateTime.parse(time.attr("datetime"), BEINSPORTS_PARSER), ZoneId.systemDefault());
     }
 
 
-    private Item getDetailsByJavascript(Item item, String urlItemBeInSport) {
+    private Item getItem(Element article) {
+        String urlItemBeInSport = String.format(beInSportsDomain, article.select("a").first().attr("data-url"));
+
         String javascriptCode;
-        try {
-            Connection.Response response = htmlService.connectWithDefault(urlItemBeInSport)
-                    .execute();
-            Document articlePage = response.parse();
-            String apiItemUrl = articlePage.select("iframe").attr("src");
+        Document document;
 
-            response = htmlService.connectWithDefault(apiItemUrl)
-                    .execute();
-            javascriptCode = getJavascriptPart(response.parse().select("script"));
+        try {
+            document = htmlService.connectWithDefault(urlItemBeInSport).execute().parse();
+            javascriptCode = getJavascriptPart(htmlService.connectWithDefault(document.select("iframe").attr("src")).execute().parse().select("script"));
         } catch (IOException | IllegalArgumentException e) {
             logger.error("Error during fetch of {}", urlItemBeInSport, e);
-            return new Item();
+            return Item.DEFAULT_ITEM;
         }
 
+        Item item = new Item()
+                .setTitle(article.select("h3").first().text())
+                .setDescription(article.select("h3").first().text())
+                .setPubdate(getPubDateFromDescription(document.select("time").first()));
 
         Matcher matcher = STREAM_720_URL_EXTRACTOR_PATTERN1.matcher(javascriptCode);
         if (matcher.find()) {
             item.setUrl(matcher.group(1).replace("\\", ""));
         }
 
-        Matcher thumNailematcher = POSTER_URL_EXTRACTOR_PATTERN.matcher(javascriptCode);
-        if (thumNailematcher.find()) {
-            item.setCover(imageService.getCoverFromURL(thumNailematcher.group(1).replace("\\", "")));
+        Matcher thumbnailMatcher = POSTER_URL_EXTRACTOR_PATTERN.matcher(javascriptCode);
+        if (thumbnailMatcher.find()) {
+            item.setCover(imageService.getCoverFromURL(thumbnailMatcher.group(1).replace("\\", "")));
         }
 
         return item;
@@ -116,7 +115,6 @@ public class BeInSportsUpdater extends AbstractUpdater {
     public String signatureOf(Podcast podcast) {
         String listingUrl = podcast.getUrl();
         /* cluster_video */
-
         try {
             Document page = htmlService.connectWithDefault(listingUrl).execute().parse();
             return signatureService.generateMD5Signature(page.select(".cluster_video").html());
