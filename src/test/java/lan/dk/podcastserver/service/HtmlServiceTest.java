79cdf3023b620d14f218089ad4abe873b31e9728
Davin Kevin <davin.kevin@gmail.com>
2016-01-04 05:22:22 +0100

    testfix(service.htmlservice): expand testing

--- src/test/java/lan/dk/podcastserver/service/HtmlServiceTest.java
+++ src/test/java/lan/dk/podcastserver/service/HtmlServiceTest.java
@@ -1,7 +1,13 @@
 package lan.dk.podcastserver.service;
 
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
 import org.jsoup.helper.HttpConnection;
+import org.jsoup.nodes.Document;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+
+import java.io.IOException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -10,13 +16,22 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 public class HtmlServiceTest {
 
+    @Rule
+    public WireMockRule wireMockRule = new WireMockRule(8089); // No-args constructor defaults to port 8080
+
     public static final String URL = "http://nowhere.anywhere";
     public static final String USER_AGENT = "User-Agent";
     public static final String REFERER = "Referer";
+    HtmlService htmlService;
+
+    @Before
+    public void beforeEach() {
+        /* Given */
+        htmlService = new HtmlService();
+    }
 
     @Test
     public void should_generate_default_connection() {
-        /* Given */ HtmlService htmlService = new HtmlService();
         /* When */ HttpConnection connection = (HttpConnection) htmlService.connectWithDefault(URL);
 
         /* Then */
@@ -27,12 +42,20 @@ public class HtmlServiceTest {
 
     @Test
     public void should_not_have_default_parameters () {
-        /* Given */ HtmlService htmlService = new HtmlService();
         /* When */ HttpConnection connection = (HttpConnection) htmlService.connect(URL);
         /* Then */
             assertThat(connection.request().header(USER_AGENT)).isNull();
             assertThat(connection.request().header(REFERER)).isNull();
             assertThat(connection.request().timeout()).isNotEqualTo(5000);
+    }
+
+    @Test
+    public void should_parse_document() throws IOException {
+        /* When */
+        Document document = htmlService.get("http://localhost:8089/service/htmlService/jsoup.html");
+
+        /* Then */
+        assertThat(document.select("title").first().text()).isEqualTo("JSOUP Example");
     }
 
 }
