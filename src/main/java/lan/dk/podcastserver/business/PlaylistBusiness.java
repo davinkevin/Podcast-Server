ddbf837e1521e8c30a09a07bd62ad12375d6b5ab
Davin Kevin <davin.kevin@gmail.com>
2016-01-17 18:45:54 +0100

    feat(playlist): add business layer

---
+++ src/main/java/lan/dk/podcastserver/business/PlaylistBusiness.java
@@ -0,0 +1,59 @@
+package lan.dk.podcastserver.business;
+
+import lan.dk.podcastserver.entity.Item;
+import lan.dk.podcastserver.entity.Playlist;
+import lan.dk.podcastserver.repository.ItemRepository;
+import lan.dk.podcastserver.repository.PlaylistRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+
+/**
+ * Created by kevin on 17/01/2016 for PodcastServer
+ */
+@Component
+public class PlaylistBusiness {
+
+    private final PlaylistRepository playlistRepository;
+    private final ItemRepository itemRepository;
+
+    @Autowired
+    public PlaylistBusiness(PlaylistRepository playlistRepository, ItemRepository itemRepository) {
+        this.playlistRepository = playlistRepository;
+        this.itemRepository = itemRepository;
+    }
+
+    public List<Playlist> findAll() {
+        return playlistRepository.findAll();
+    }
+
+    public Set<Playlist> findContainsItem(Integer itemId) {
+        Item item = itemRepository.findOne(itemId);
+        return playlistRepository.findContainsItem(item);
+    }
+
+    public Playlist add(UUID playlistId, Integer itemId) {
+        Playlist playlist = playlistRepository.findOne(playlistId);
+        Item item = itemRepository.findOne(itemId);
+
+        return playlistRepository.save(playlist.add(item));
+    }
+
+    public Playlist remove(UUID playlistId, Integer itemId) {
+        Playlist playlist = playlistRepository.findOne(playlistId);
+        Item item = itemRepository.findOne(itemId);
+
+        return playlistRepository.save(playlist.remove(item));
+    }
+
+    public void delete(UUID uuid) {
+        playlistRepository.delete(uuid);
+    }
+
+    public Playlist save(Playlist entity) {
+        return playlistRepository.save(entity);
+    }
+}
