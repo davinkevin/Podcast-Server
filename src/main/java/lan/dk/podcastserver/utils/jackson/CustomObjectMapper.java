2556dcd0110fab2b80e286bd16a2acb501fe0ce0
Davin Kevin <davin.kevin@gmail.com>
2016-01-17 19:28:57 +0100

    feat(playlist): active lazy loading on item

--- src/main/java/lan/dk/podcastserver/utils/jackson/CustomObjectMapper.java
+++ src/main/java/lan/dk/podcastserver/utils/jackson/CustomObjectMapper.java
@@ -17,7 +17,8 @@ public class CustomObjectMapper extends ObjectMapper {
 
     private Hibernate4Module hibernate4Module() {
         return new Hibernate4Module()
-                .configure(Hibernate4Module.Feature.USE_TRANSIENT_ANNOTATION, false);
+                .configure(Hibernate4Module.Feature.USE_TRANSIENT_ANNOTATION, false)
+                .configure(Hibernate4Module.Feature.FORCE_LAZY_LOADING, true);
     }
 
 }
