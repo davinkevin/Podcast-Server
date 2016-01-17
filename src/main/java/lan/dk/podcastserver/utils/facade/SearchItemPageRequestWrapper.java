87b35be8f80b1567517b483872ab77a3863b5151
Davin Kevin <davin.kevin@gmail.com>
2016-01-17 10:39:23 +0100

    refactor(facade.SearchItemPageRequestWrapper): use getter and setter from lombok project to simplify code

--- src/main/java/lan/dk/podcastserver/utils/facade/SearchItemPageRequestWrapper.java
+++ src/main/java/lan/dk/podcastserver/utils/facade/SearchItemPageRequestWrapper.java
@@ -1,6 +1,9 @@
 package lan.dk.podcastserver.utils.facade;
 
 import lan.dk.podcastserver.entity.Tag;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.data.domain.Sort;
 
@@ -12,6 +15,8 @@ import static java.util.Objects.nonNull;
 /**
  * Created by kevin on 08/06/2014 for Podcast Server
  */
+@Getter @Setter
+@Accessors(chain = true)
 public class SearchItemPageRequestWrapper extends PageRequestFacade {
 
     private List<Tag> tags = new ArrayList<>();
@@ -21,31 +26,6 @@ public class SearchItemPageRequestWrapper extends PageRequestFacade {
     public SearchItemPageRequestWrapper() {
         this.orders.add(new OrderFacade(Sort.Direction.DESC.toString(), "pubdate"));
         this.downloaded = Boolean.TRUE;
-    }
-
-    public List<Tag> getTags() {
-        return tags;
-    }
-    public SearchItemPageRequestWrapper setTags(List<Tag> tags) {
-        this.tags = tags;
-        return this;
-    }
-
-    public String getTerm() {
-        return term;
-    }
-    public SearchItemPageRequestWrapper setTerm(String term) {
-        this.term = term;
-        return this;
-    }
-
-    public SearchItemPageRequestWrapper downloaded(Boolean downloaded) {
-        this.downloaded = downloaded;
-        return this;
-    }
-
-    public Boolean getDownloaded() {
-        return downloaded;
     }
 
     public Boolean isSearch() {
