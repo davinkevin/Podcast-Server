c50d3d429029ef0bce200de679248217a2a1855d
Davin Kevin <davin.kevin@gmail.com>
2016-01-25 05:23:54 +0100

    refactor(downloader.http): externalize downloader logic in a Watcher

--- src/main/java/lan/dk/podcastserver/manager/worker/downloader/HTTPDownloader.java
+++ src/main/java/lan/dk/podcastserver/manager/worker/downloader/HTTPDownloader.java
@@ -5,18 +5,28 @@ import com.github.axet.wget.info.DownloadInfo;
 import com.github.axet.wget.info.ex.DownloadInterruptedError;
 import com.github.axet.wget.info.ex.DownloadMultipartError;
 import lan.dk.podcastserver.entity.Item;
-import lan.dk.podcastserver.utils.URLUtils;
+import lan.dk.podcastserver.manager.ItemDownloadManager;
+import lan.dk.podcastserver.service.UrlService;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.FilenameUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Objects;
+
+import static java.util.Objects.isNull;
+import static java.util.Objects.nonNull;
 
 @Scope("prototype")
 @Component("HTTPDownloader")
 public class HTTPDownloader extends AbstractDownloader {
-    protected DownloadInfo info = null;
+
+    @Autowired UrlService urlService;
+
+    DownloadInfo info = null;
 
     @Override
     public Item download() {
@@ -25,54 +35,10 @@ public class HTTPDownloader extends AbstractDownloader {
         //this.startDownload();
         //int borne = randomGenerator.nextInt(100);
         try {
-            URL url = new URL(URLUtils.getRealURL(getItemUrl()));
             // initialize url information object
-            info = new DownloadInfo(url);
+            info = new DownloadInfo(new URL(urlService.getRealURL(getItemUrl())));
             // extract infromation from the web
-            Runnable itemSynchronisation = new Runnable() {
-                long last;
-                @Override
-                public void run() {
-                    // notify app or save download state
-                    // you can extract information from DownloadInfo info;
-                    switch (info.getState()) {
-                        case EXTRACTING:
-                        case EXTRACTING_DONE:
-                            logger.debug(FilenameUtils.getName(String.valueOf(getItemUrl())) + " " + info.getState());
-                            break;
-                        case ERROR:
-                            stopDownload();
-                            break;
-                        case DONE:
-                            logger.debug(FilenameUtils.getName(String.valueOf(getItemUrl())) + " - Téléchargement terminé");
-                            finishDownload();
-                            itemDownloadManager.removeACurrentDownload(item);
-                            break;
-                        case RETRYING:
-                            logger.debug(FilenameUtils.getName(String.valueOf(getItemUrl())) + " " + info.getState() + " " + info.getDelay());
-                            break;
-                        case DOWNLOADING:
-                            long now = System.currentTimeMillis();
-                            int progression = 0;
-                            if (now - 1000 > last && info.getLength() != null && info.getLength() != 0L) {
-                                last = now;
-                                progression = (int) (info.getCount()*100 / (float) info.getLength());
-                                if (item.getProgression() < progression) {
-                                    item.setProgression(progression);
-                                    logger.debug("Progression de {} : {}%", item.getTitle(), progression);
-                                    convertAndSaveBroadcast();
-                                }
-                            }
-                            break;
-                        case STOP:
-                            logger.debug("Pause / Arrêt du téléchargement du téléchargement");
-                            //stopDownload();
-                            break;
-                        default:
-                            break;
-                    }
-                }
-            };
+            Runnable itemSynchronisation = new HTTPWatcher(this);
 
             info.extract(stopDownloading, itemSynchronisation);
             target = getTagetFile(this.item);
@@ -82,22 +48,70 @@ public class HTTPDownloader extends AbstractDownloader {
             // will blocks until download finishes
             w.download(stopDownloading, itemSynchronisation);
         } catch (DownloadMultipartError e) {
-            for (DownloadInfo.Part p : e.getInfo().getParts()) {
-                Throwable ee = p.getException();
-                if (ee != null)
-                    ee.printStackTrace();
-            }
+            e.getInfo().getParts()
+                .stream()
+                .map(DownloadInfo.Part::getException)
+                .filter(Objects::nonNull)
+                .forEach(Throwable::printStackTrace);
+
             stopDownload();
         } catch (DownloadInterruptedError e) {
             logger.debug("Arrêt du téléchargement");
-            //stopDownload();
         } catch (IOException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            logger.debug("Exception during download", e);
             stopDownload();
         }
-        //logger.debug("Download termine");
-        //finishDownload();
-        //itemDownloadManager.removeACurrentDownload(item);
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        return item;
+    }
+
+    @Slf4j
+    static class HTTPWatcher implements Runnable {
+
+        final HTTPDownloader httpDownloader;
+
+        public HTTPWatcher(HTTPDownloader httpDownloader) {
+            this.httpDownloader = httpDownloader;
+        }
+
+        @Override
+        public void run() {
+
+            DownloadInfo info = httpDownloader.info;
+            ItemDownloadManager itemDownloadManager = httpDownloader.itemDownloadManager;
+            Item item = httpDownloader.item;
+
+            switch (info.getState()) {
+                case EXTRACTING:
+                case EXTRACTING_DONE:
+                    log.debug(FilenameUtils.getName(String.valueOf(httpDownloader.getItemUrl())) + " " + info.getState());
+                    break;
+                case ERROR:
+                    httpDownloader.stopDownload();
+                    break;
+                case DONE:
+                    log.debug(FilenameUtils.getName(String.valueOf(httpDownloader.getItemUrl())) + " - Téléchargement terminé");
+                    httpDownloader.finishDownload();
+                    itemDownloadManager.removeACurrentDownload(item);
+                    break;
+                case RETRYING:
+                    log.debug(FilenameUtils.getName(String.valueOf(httpDownloader.getItemUrl())) + " " + info.getState() + " " + info.getDelay());
+                    break;
+                case DOWNLOADING:
+                    if (isNull(info.getLength()) || (nonNull(info.getLength()) && info.getLength() == 0L)) break;
+
+                    int progression = (int) (info.getCount()*100 / (float) info.getLength());
+                    if (item.getProgression() < progression) {
+                        item.setProgression(progression);
+                        log.debug("Progression de {} : {}%", item.getTitle(), progression);
+                        httpDownloader.convertAndSaveBroadcast();
+                    }
+                    break;
+                case STOP:
+                    log.debug("Pause / Arrêt du téléchargement du téléchargement");
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 }
