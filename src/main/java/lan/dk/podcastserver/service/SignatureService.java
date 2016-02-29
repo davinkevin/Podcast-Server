b6caff6ec3bdf1a4c2c0a5f81eca5ec4c4ab80d2
Davin Kevin <davin.kevin@gmail.com>
2016-02-29 05:39:33 +0100

    feat(signature): use auto-closeable for Stream

--- src/main/java/lan/dk/podcastserver/service/SignatureService.java
+++ src/main/java/lan/dk/podcastserver/service/SignatureService.java
@@ -1,30 +1,30 @@
 package lan.dk.podcastserver.service;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+
+import static java.util.stream.Collectors.joining;
 
 /**
  * Created by kevin on 24/01/15 for Podcast Server
  */
 @Slf4j
 @Service
+@RequiredArgsConstructor(onConstructor = @__(@Autowired) )
 public class SignatureService {
 
     final UrlService urlService;
 
-    @Autowired
-    public SignatureService(UrlService urlService) {
-        this.urlService = urlService;
-    }
-
     public String generateSignatureFromURL(String urlAsString) {
-        try {
-            return DigestUtils.md5Hex(urlService.getConnection(urlAsString).getInputStream());
+        try(BufferedReader in = urlService.urlAsReader(urlAsString)) {
+            return generateMD5Signature(in.lines().collect(joining()));
         } catch (IOException e) {
             log.error("Error during signature of podcast at url {}", urlAsString, e);
         }
--- src/test/java/lan/dk/podcastserver/service/SignatureServiceTest.java
+++ src/test/java/lan/dk/podcastserver/service/SignatureServiceTest.java
@@ -7,14 +7,13 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
-import java.io.ByteArrayInputStream;
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.net.URLConnection;
+import java.io.StringReader;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 /**
@@ -30,9 +29,7 @@ public class SignatureServiceTest {
     public void should_generate_md5_from_stream() throws IOException {
         /* Given */
         String stringStream = "azertyuiopqsdfghjklmwxcvbn";
-        URLConnection connection = mock(URLConnection.class);
-        when(urlService.getConnection(anyString())).thenReturn(connection);
-        when(connection.getInputStream()).thenReturn(new ByteArrayInputStream(stringStream.getBytes()));
+        when(urlService.urlAsReader(anyString())).thenReturn(new BufferedReader(new StringReader(stringStream)));
 
         /* When */
         String s = signatureService.generateSignatureFromURL("");
@@ -44,8 +41,7 @@ public class SignatureServiceTest {
     @Test
     public void should_return_empty_string_if_error_during_connection() throws IOException {
         /* Given */
-        URLConnection connection = mock(URLConnection.class);
-        doThrow(IOException.class).when(urlService).getConnection(anyString());
+        doThrow(IOException.class).when(urlService).urlAsReader(anyString());
 
         /* When */
         String s = signatureService.generateSignatureFromURL("");
