bebda67d460c850a124954fad1f628a4cf88a694
Davin Kevin <davin.kevin@gmail.com>
2016-03-31 05:51:56 +0200

    test(downloader.rtmp): expand testing

--- src/main/java/lan/dk/podcastserver/service/factory/ProcessBuilderFactory.java
+++ src/main/java/lan/dk/podcastserver/service/factory/ProcessBuilderFactory.java
@@ -1,12 +1,15 @@
 package lan.dk.podcastserver.service.factory;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.lang.reflect.Field;
 import java.util.List;
 
 /**
  * Created by kevin on 25/01/2016 for Podcast Server
  */
+@Slf4j
 @Service
 public class ProcessBuilderFactory {
 
@@ -15,4 +18,14 @@ public class ProcessBuilderFactory {
         return new ProcessBuilder(command);
     }
 
+    public int pidOf(Process p) {
+        try {
+            if (p.getClass().getSimpleName().contains("UNIXProcess")) {
+                Field pidField = p.getClass().getDeclaredField("pid");
+                pidField.setAccessible(true);
+                return pidField.getInt(p);
+            }
+        } catch (IllegalAccessException | NoSuchFieldException ignored) {}
+        return -1;
+    }
 }
