ef3d206f68e0594ecafffca5020ce9a8d5c49ecd
Davin Kevin <davin.kevin@gmail.com>
2016-04-01 05:40:29 +0200

    feat(finder.dailymotion): add finder for dailymotion
    Linked to #14

--- src/main/java/lan/dk/podcastserver/manager/worker/finder/DailymotionFinder.java
+++ src/main/java/lan/dk/podcastserver/manager/worker/finder/DailymotionFinder.java
@@ -8,6 +8,7 @@ import lan.dk.podcastserver.service.JsonService;
 import lan.dk.podcastserver.service.UrlService;
 import lombok.RequiredArgsConstructor;
 import org.hibernate.validator.constraints.NotEmpty;
+import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -24,8 +25,8 @@ import static java.util.Objects.nonNull;
 @RequiredArgsConstructor(onConstructor = @__(@Autowired) )
 public class DailymotionFinder implements Finder {
 
-    public static final String API_URL = "https://api.dailymotion.com/user/%s?fields=avatar_720_url,description,username";
-    public static final Pattern USER_NAME_EXTRACTOR = Pattern.compile("^.+dailymotion.com\\/(.*)");
+    private static final String API_URL = "https://api.dailymotion.com/user/%s?fields=avatar_720_url,description,username";
+    private static final Pattern USER_NAME_EXTRACTOR = Pattern.compile("^.+dailymotion.com\\/(.*)");
 
     final JsonService jsonService;
     final ImageService imageService;
@@ -37,14 +38,18 @@ public class DailymotionFinder implements Finder {
                 .map(username -> String.format(API_URL, username))
                 .flatMap(urlService::newURL)
                 .flatMap(jsonService::from)
-                .map(o -> Podcast.builder()
-                        .cover(imageService.getCoverFromURL((String) o.get("avatar_720_url")))
-                        .url(url)
-                        .title(((String) o.get("username")))
-                        .description(((String) o.get("description")))
-                        .type("Dailymotion")
-                        .build())
+                .map(o -> jsonToPodcast(url, o))
                 .orElse(Podcast.builder().url(url).type("Dailymotion").cover(new Cover()).build());
+    }
+
+    private Podcast jsonToPodcast(String url, JSONObject o) {
+        return Podcast.builder()
+                .cover(imageService.getCoverFromURL((String) o.get("avatar_720_url")))
+                .url(url)
+                .title(((String) o.get("username")))
+                .description(((String) o.get("description")))
+                .type("Dailymotion")
+                .build();
     }
 
     private Optional<String> usernameOf(String url) {
---
+++ src/test/java/lan/dk/podcastserver/manager/worker/finder/DailymotionFinderTest.java
@@ -0,0 +1,94 @@
+package lan.dk.podcastserver.manager.worker.finder;
+
+import lan.dk.podcastserver.entity.Cover;
+import lan.dk.podcastserver.entity.Podcast;
+import lan.dk.podcastserver.entity.PodcastAssert;
+import lan.dk.podcastserver.service.ImageService;
+import lan.dk.podcastserver.service.JsonService;
+import lan.dk.podcastserver.service.UrlService;
+import org.json.simple.parser.JSONParser;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.stubbing.Answer;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.when;
+
+/**
+ * Created by kevin on 21/03/2016 for Podcast Server
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class DailymotionFinderTest {
+
+    private static final JSONParser PARSER = new JSONParser();
+    @Mock JsonService jsonService;
+    @Mock ImageService imageService;
+    @Mock UrlService urlService;
+    @InjectMocks DailymotionFinder dailymotionFinder;
+
+    @Before
+    public void beforeEach() {
+        when(urlService.newURL(anyString())).then(i -> Optional.of(new URL((String) i.getArguments()[0])));
+    }
+
+    @Test
+    public void should_find_podcast() throws MalformedURLException {
+        /* Given */
+        String url = "http://www.dailymotion.com/karimdebbache";
+        Cover cover = new Cover("http://s2.dmcdn.net/PB4mc/720x720-AdY.jpg", 200, 200);
+        when(imageService.getCoverFromURL(eq("http://s2.dmcdn.net/PB4mc/720x720-AdY.jpg"))).thenReturn(cover);
+        when(jsonService.from(eq(new URL("https://api.dailymotion.com/user/karimdebbache?fields=avatar_720_url,description,username")))).then(readerFrom("karimdebbache"));
+
+        /* When */
+        Podcast podcast = dailymotionFinder.find(url);
+
+        /* Then */
+        PodcastAssert
+                .assertThat(podcast)
+                .hasDescription("CHROMA est une CHROnique de cinéMA sur Dailymotion, dont la première saison se compose de dix épisodes, à raison d’un par mois, d’une durée comprise entre quinze et vingt minutes. Chaque épisode est consacré à un film en particulier.")
+                .hasCover(cover)
+                .hasTitle("karimdebbache")
+                .hasType("Dailymotion");
+    }
+    
+    @Test
+    public void should_not_find_podcast() {
+        /* Given */
+        String url = "http://iojafea/fake/url";
+        /* When */
+        Podcast podcast = dailymotionFinder.find(url);
+        /* Then */
+        PodcastAssert
+                .assertThat(podcast)
+                .hasUrl(url)
+                .hasType("Dailymotion")
+                .hasCover(new Cover());
+    }
+
+    @Test
+    public void should_be_compatible() {
+        assertThat(dailymotionFinder.compatibility("http://www.dailymotion.com/karimdebbache")).isEqualTo(1);
+    }
+
+    @Test
+    public void should_not_be_compatible() {
+        assertThat(dailymotionFinder.compatibility("http://iojafea/fake/url")).isGreaterThan(1);
+    }
+
+    private Answer<Optional<Object>> readerFrom(String url) {
+        return i -> Optional.of(PARSER.parse(Files.newBufferedReader(Paths.get(DailymotionFinderTest.class.getResource("/remote/podcast/dailymotion/" + url + ".json").toURI()))));
+    }
+
+}
