7b3e5a10297ab8d711c659bfc21132cf15f4bfd2
Davin Kevin <davin.kevin@gmail.com>
2016-04-13 05:32:23 +0200

    test(config): expand testing

--- src/main/java/lan/dk/podcastserver/config/BeanConfigScan.java
+++ src/main/java/lan/dk/podcastserver/config/BeanConfigScan.java
@@ -1,28 +1,52 @@
 package lan.dk.podcastserver.config;
 
+import lan.dk.podcastserver.service.PodcastServerParameters;
+import lan.dk.podcastserver.service.properties.Api;
+import lan.dk.podcastserver.service.properties.Backup;
+import lan.dk.podcastserver.service.properties.ExternalTools;
 import org.apache.tika.Tika;
+import org.springframework.boot.context.properties.ConfigurationPropertiesBinding;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import static lan.dk.podcastserver.service.MimeTypeService.TikaProbeContentType;
+import static org.aspectj.weaver.tools.cache.SimpleCacheFactory.path;
 
 /**
  * Created by kevin on 26/12/2013.
  */
+@EnableCaching
 @Configuration
+@EnableConfigurationProperties({PodcastServerParameters.class, Api.class, Backup.class, ExternalTools.class})
 @ComponentScan(basePackages = { "lan.dk.podcastserver.utils", "lan.dk.podcastserver.service", "lan.dk.podcastserver.business"})
 public class BeanConfigScan {
 
     @Bean(name="Validator")
-    public LocalValidatorFactoryBean validator() {
+    LocalValidatorFactoryBean validator() {
         return new LocalValidatorFactoryBean();
     }
 
+    @Bean TikaProbeContentType tikaProbeContentType() {
+        return new TikaProbeContentType(new Tika());
+    }
 
     @Bean
-    public TikaProbeContentType tikaProbeContentType() {
-        return new TikaProbeContentType(new Tika());
+    @ConfigurationPropertiesBinding
+    Converter<String, Path> pathConverter() {
+        return new Converter<String, Path>(){
+
+            @Override
+            public Path convert(String source) {
+                return Paths.get(path);
+            }
+        };
     }
 }
--- src/test/java/lan/dk/podcastserver/config/BeanConfigScanTest.java
+++ src/test/java/lan/dk/podcastserver/config/BeanConfigScanTest.java
@@ -1,8 +1,14 @@
 package lan.dk.podcastserver.config;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static lan.dk.podcastserver.service.MimeTypeService.TikaProbeContentType;
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
@@ -10,15 +16,46 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 public class BeanConfigScanTest {
 
+    private BeanConfigScan beanConfigScan;
+
+    @Before
+    public void beforeEach() {
+        beanConfigScan = new BeanConfigScan();
+    }
+
     @Test
     public void should_get_validator() {
         /* Given */
-        BeanConfigScan beanConfigScan = new BeanConfigScan();
         /* When */
         LocalValidatorFactoryBean validator = beanConfigScan.validator();
         /* Then */
         assertThat(validator)
                 .isNotNull()
                 .isOfAnyClassIn(LocalValidatorFactoryBean.class);
+    }
+
+    @Test
+    public void should_have_tika_probecontentType() throws NoSuchMethodException {
+        /* Given */
+        /* When */
+        TikaProbeContentType tikaProbeContentType = beanConfigScan.tikaProbeContentType();
+
+        /* Then */
+        assertThat(tikaProbeContentType)
+                .isNotNull()
+                .isOfAnyClassIn(TikaProbeContentType.class);
+    }
+
+    @Test
+    public void should_provide_a_converter_from_string_to_path() {
+        /* Given */
+        Converter<String, Path> c = beanConfigScan.pathConverter();
+        String path = "/tmp";
+
+        /* When */
+        Path convertedPath = c.convert(path);
+
+        /* Then */
+        assertThat(convertedPath).isEqualTo(Paths.get(path));
     }
 }
