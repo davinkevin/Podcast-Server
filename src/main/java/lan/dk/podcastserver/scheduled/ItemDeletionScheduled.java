3485c62fa57dccc07dd3fee9e5b2fff9738d03ac
Davin Kevin <davin.kevin@gmail.com>
2016-04-23 14:43:38 +0200

    refactor(shceduled.deleting): use lombok to reduce code

--- src/main/java/lan/dk/podcastserver/scheduled/ItemDeletionScheduled.java
+++ src/main/java/lan/dk/podcastserver/scheduled/ItemDeletionScheduled.java
@@ -1,18 +1,19 @@
 package lan.dk.podcastserver.scheduled;
 
 import lan.dk.podcastserver.business.update.UpdatePodcastBusiness;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-
-import javax.annotation.Resource;
 
 /**
  * Created by kevin on 26/12/2013.
  */
 @Component
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class ItemDeletionScheduled {
 
-    @Resource UpdatePodcastBusiness updatePodcastBusiness;
+    final UpdatePodcastBusiness updatePodcastBusiness;
 
     @Scheduled(fixedDelay = 86400000)
     public void deleteOldItem() {
