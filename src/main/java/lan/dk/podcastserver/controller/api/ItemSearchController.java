5d1306fa1dde1b44c2919a959cbec7db088f6986
Davin Kevin <davin.kevin@gmail.com>
2016-04-28 05:46:04 +0200

    refactor(controller.itemSearch): refactor to use lombok for constructor

--- src/main/java/lan/dk/podcastserver/controller/api/ItemSearchController.java
+++ src/main/java/lan/dk/podcastserver/controller/api/ItemSearchController.java
@@ -5,21 +5,24 @@ import lan.dk.podcastserver.business.ItemBusiness;
 import lan.dk.podcastserver.entity.Item;
 import lan.dk.podcastserver.utils.facade.PageRequestFacade;
 import lan.dk.podcastserver.utils.facade.SearchItemPageRequestWrapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-
-import javax.annotation.Resource;
 
 /**
  * Created by kevin on 23/08/2014.
  * Podcast Server
  */
+@Slf4j
 @RestController
 @RequestMapping("/api/item")
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class ItemSearchController {
 
-    @Resource private ItemBusiness itemBusiness;
+    final private ItemBusiness itemBusiness;
 
     @RequestMapping(value="pagination", method = RequestMethod.GET)
     @JsonView(Item.ItemSearchListView.class)
@@ -27,7 +30,7 @@ public class ItemSearchController {
         return itemBusiness.findAll(pageRequestFacade.toPageRequest());
     }
 
-    @RequestMapping(value= {"search"}, method = RequestMethod.POST )
+    @RequestMapping(value= "search", method = RequestMethod.POST )
     @JsonView(Item.ItemSearchListView.class)
     public Page<Item> search(@RequestBody SearchItemPageRequestWrapper searchWrapper) {
         return searchWrapper.isSearch()
