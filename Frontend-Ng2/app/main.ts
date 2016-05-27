53b1b532d458b71e178c28fdeb91d13a9e0408d8
Davin Kevin <davin.kevin@gmail.com>
2016-05-27 06:07:57 +0200

    chore(frontend-ng2): add startup project for whole refreshment of Angular2 IHM
    Linked to #15

---
+++ Frontend-Ng2/app/main.ts
@@ -0,0 +1,7 @@
+/**
+ * Created by kdavin on 10/05/2016.
+ */
+import { bootstrap } from '@angular/platform-browser-dynamic';
+import { PodcastServerApp } from './podcast-server';
+
+bootstrap(PodcastServerApp);
---
+++ Frontend-Ng2/app/podcast-server.ts
@@ -0,0 +1,10 @@
+/**
+ * Created by kdavin on 10/05/2016.
+ */
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'podcast-server',
+    template: '<h1>Podcast-Server on NG2</h1>'
+})
+export class PodcastServerApp { }
