305085a727c14370f445c06d2b37e307725be512
davinkevin <davin.kevin@gmail.com>
2016-07-15 15:22:22 +0200

    test(startup): start the application as a test

---
+++ Backend/src/test/java/lan/dk/podcastserver/ApplicationTest.java
@@ -0,0 +1,59 @@
+package lan.dk.podcastserver;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lan.dk.podcastserver.service.JsonService;
+import lombok.Getter;
+import lombok.Setter;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.io.IOException;
+import java.net.URL;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Created by kevin on 15/06/2016 for Podcast Server
+ */
+@Ignore("Should have FFmpeg installed to be executed")
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+@WebIntegrationTest("server.port:9000")
+public class ApplicationTest {
+
+    @Autowired JsonService jsonService;
+
+    @Test
+    public void should_respond_true_to_health_check() throws IOException {
+        /* Given */
+        URL health = new URL("http://localhost:9000/system/health");
+        /* When */
+        ApplicationHealthStatus status = jsonService.parse(health).map(d -> d.read("$", ApplicationHealthStatus.class)).orElseThrow(RuntimeException::new);
+        /* Then */
+        assertThat(status).isNotNull();
+        assertThat(status.getStatus()).isEqualTo("UP");
+        assertThat(status.getDb().getStatus()).isEqualTo("UP");
+        assertThat(status.getDiskSpace().getStatus()).isEqualTo("UP");
+    }
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class ApplicationHealthStatus extends HealthStatus {
+        @Getter @Setter private DiskSpaceStatus diskSpace;
+        @Getter @Setter private DbStatus db;
+
+        @JsonIgnoreProperties(ignoreUnknown = true)
+        public static class DiskSpaceStatus extends HealthStatus {}
+        @JsonIgnoreProperties(ignoreUnknown = true)
+        public static class DbStatus extends HealthStatus {}
+    }
+
+    public static class HealthStatus {
+        @Getter @Setter private String status;
+    }
+
+}
