14871c8204732e9d6fb18f37b82e810f8d0dfe50
davinkevin <davin.kevin@gmail.com>
2016-07-28 09:33:13 +0200

    refactor(controller): use di by contructor in task controller

--- Backend/src/main/java/lan/dk/podcastserver/controller/task/UpdatePodcastController.java
+++ Backend/src/main/java/lan/dk/podcastserver/controller/task/UpdatePodcastController.java
@@ -2,21 +2,22 @@ package lan.dk.podcastserver.controller.task;
 
 import lan.dk.podcastserver.business.update.UpdatePodcastBusiness;
 import lan.dk.podcastserver.manager.ItemDownloadManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.Resource;
-
 
 @RestController
 @RequestMapping("/api/task/updateManager")
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class UpdatePodcastController {
 
-    @Resource UpdatePodcastBusiness updatePodcastBusiness;
-    @Resource ItemDownloadManager IDM;
+    final UpdatePodcastBusiness updatePodcastBusiness;
+    final ItemDownloadManager IDM;
 
     @RequestMapping(value = "/updatePodcast", method = RequestMethod.GET)
     @ResponseStatus(value = HttpStatus.NO_CONTENT)
