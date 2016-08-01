c79a9a015d070292f7045b36ea6a280084c2d14e
davinkevin <davin.kevin@gmail.com>
2016-08-01 04:58:06 +0200

    refactor(controller): use di with constructor

--- Backend/src/main/java/lan/dk/podcastserver/controller/ws/DownloadControllerWS.java
+++ Backend/src/main/java/lan/dk/podcastserver/controller/ws/DownloadControllerWS.java
@@ -2,21 +2,22 @@ package lan.dk.podcastserver.controller.ws;
 
 import lan.dk.podcastserver.entity.Item;
 import lan.dk.podcastserver.manager.ItemDownloadManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.annotation.SubscribeMapping;
 import org.springframework.stereotype.Controller;
 
-import javax.annotation.Resource;
 import java.util.Collection;
 
 /**
  * Created by kevin on 27/06/2014.
  */
 @Controller
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class DownloadControllerWS {
 
-    @Resource
-    ItemDownloadManager IDM;
+    final ItemDownloadManager IDM;
 
     @SubscribeMapping("/waiting")
     public Collection<Item> waitingList() {
--- Backend/src/main/java/lan/dk/podcastserver/controller/ws/UpdateControllerWS.java
+++ Backend/src/main/java/lan/dk/podcastserver/controller/ws/UpdateControllerWS.java
@@ -1,18 +1,19 @@
 package lan.dk.podcastserver.controller.ws;
 
 import lan.dk.podcastserver.business.update.UpdatePodcastBusiness;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.simp.annotation.SubscribeMapping;
 import org.springframework.stereotype.Controller;
-
-import javax.annotation.Resource;
 
 /**
  * Created by kevin on 15/05/15 for HackerRank problem
  */
 @Controller
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class UpdateControllerWS {
 
-    @Resource UpdatePodcastBusiness updatePodcastBusiness;
+    final UpdatePodcastBusiness updatePodcastBusiness;
 
     @SubscribeMapping("/updating")
     public Boolean isUpdating() {
