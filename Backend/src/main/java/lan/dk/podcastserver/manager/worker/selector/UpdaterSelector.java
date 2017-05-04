ebbc635326ae7c2c86c3c5dde1f09ded03578230
davinkevin <davin.kevin@gmail.com>
2017-05-04 05:18:08 +0200

    refactor(updater): use javaslang.Set structure instead of simple java Set for types

--- Backend/src/main/java/lan/dk/podcastserver/manager/worker/selector/UpdaterSelector.java
+++ Backend/src/main/java/lan/dk/podcastserver/manager/worker/selector/UpdaterSelector.java
@@ -5,7 +5,6 @@ import javaslang.collection.Set;
 import lan.dk.podcastserver.manager.worker.updater.AbstractUpdater;
 import lan.dk.podcastserver.manager.worker.updater.NoOpUpdater;
 import lan.dk.podcastserver.manager.worker.updater.Updater;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
@@ -15,26 +14,28 @@ import java.util.Comparator;
  * Created by kevin on 06/03/15.
  */
 @Service
-@RequiredArgsConstructor
 public class UpdaterSelector {
 
     static final NoOpUpdater NO_OP_UPDATER = new NoOpUpdater();
 
-    private final java.util.Set<Updater> updaters;
-    
+    private final Set<Updater> updaters;
+
+    public UpdaterSelector(java.util.Set<Updater> updaters) {
+        this.updaters = HashSet.ofAll(updaters);
+    }
+
     public Updater of(String url) {
         if (StringUtils.isEmpty(url)) {
             return NO_OP_UPDATER;
         }
         
         return updaters
-                .stream()
-                .min(Comparator.comparing(updater -> updater.compatibility(url)))
-                .orElse(NO_OP_UPDATER);
+                .minBy(Comparator.comparing(updater -> updater.compatibility(url)))
+                .getOrElse(NO_OP_UPDATER);
     }
 
     public Set<AbstractUpdater.Type> types() {
-        return HashSet.ofAll(updaters).map(Updater::type);
+        return updaters.map(Updater::type);
     }
 
 
