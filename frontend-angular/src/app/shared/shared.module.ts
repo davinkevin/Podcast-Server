6ef82ee40195398048ecc5b3be4d21301661b473
davinkevin <davin.kevin@gmail.com>
2017-06-20 22:51:33 +0200

    refactor(shared): create the shared module at root level

---
+++ frontend-angular/src/app/shared/shared.module.ts
@@ -0,0 +1,8 @@
+import {NgModule} from '@angular/core';
+import {ItemService} from './service/item/item.service';
+
+@NgModule({
+  imports: [],
+  providers: [ItemService]
+})
+export class SharedModule { }
