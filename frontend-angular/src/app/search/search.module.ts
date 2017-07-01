38a361f2060b0c85f70428ce2581b729fd3e958b
davinkevin <davin.kevin@gmail.com>
2017-07-01 23:01:10 +0200

    feat(search): show items

--- frontend-angular/src/app/search/search.module.ts
+++ frontend-angular/src/app/search/search.module.ts
@@ -1,9 +1,10 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 import {SearchComponent} from './search.component';
 import {SharedModule} from '../shared/shared.module';
 import {RouterModule, Routes} from '@angular/router';
 import {SearchResolver} from './resolver/search.resolver';
+import {MdButtonModule, MdCardModule, MdIconModule} from '@angular/material';
 
 const routes: Routes = [
   { path: 'search',
@@ -15,7 +16,8 @@ const routes: Routes = [
 @NgModule({
   imports: [
     CommonModule, SharedModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    MdCardModule, MdButtonModule, MdIconModule
   ],
   providers: [SearchResolver],
   exports: [SearchComponent],
