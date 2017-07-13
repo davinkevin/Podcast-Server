2d3733d761788778ab557f83c1f8002336ae8a1a
davinkevin <davin.kevin@gmail.com>
2017-07-13 06:13:50 +0200

    refactor(core): remove useless interface in `app.component`

--- frontend-angular/src/app/app.component.ts
+++ frontend-angular/src/app/app.component.ts
@@ -5,7 +5,4 @@ import {Component, OnInit} from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  constructor() {}
-  ngOnInit(): void {}
-}
+export class AppComponent{}
