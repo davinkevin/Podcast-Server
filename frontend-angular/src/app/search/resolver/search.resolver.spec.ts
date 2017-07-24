de0ec586f4d98c6740a3a74b8ed420f69b4ff210
davinkevin <davin.kevin@gmail.com>
2017-07-24 05:28:13 +0200

    test(front): expand test to resolver

--- frontend-angular/src/app/search/resolver/search.resolver.spec.ts
+++ frontend-angular/src/app/search/resolver/search.resolver.spec.ts
@@ -1,15 +1,39 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { SearchResolver } from './search.resolver';
+import {ItemService} from '../../shared/service/item/item.service';
+import {ActivatedRouteSnapshot} from '@angular/router';
+import Spy = jasmine.Spy;
+import {Observable} from 'rxjs/Observable';
 
-xdescribe('SearchResolver', () => {
+describe('SearchResolver', () => {
+
+  let itemService: ItemService;
+  let route: ActivatedRouteSnapshot;
+
+  beforeEach(() => {
+    itemService = jasmine.createSpyObj('itemService', ['search']);
+    (itemService.search as Spy).and.returnValue(Observable.of({}));
+  });
+
+  beforeEach(() => {
+    route = <ActivatedRouteSnapshot> {
+      queryParamMap: jasmine.createSpyObj('queryParamMap', ['get'])
+    };
+    (route.queryParamMap.get as Spy).and.returnValue(3);
+  });
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [SearchResolver]
+      providers: [
+        SearchResolver,
+        {provide: ItemService, useValue: itemService},
+        {provide: ActivatedRouteSnapshot, useValue: route},
+      ]
     });
   });
 
-  it('should ...', inject([SearchResolver], (service: SearchResolver) => {
+  it('should be able to inject service', inject([SearchResolver], (service: SearchResolver) => {
     expect(service).toBeTruthy();
   }));
 });
