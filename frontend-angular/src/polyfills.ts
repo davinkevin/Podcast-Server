69c8714ead12ab5305c49a8eadee081fff9a9f58
davinkevin <davin.kevin@gmail.com>
2018-03-12 05:50:04 +0100

    chore(ng): move to @angular/cli 1.6.8

--- frontend-angular/src/polyfills.ts
+++ frontend-angular/src/polyfills.ts
@@ -51,18 +51,6 @@ import 'core-js/es7/reflect';
 
 
 /***************************************************************************************************
- * Zone JS is required by Angular itself.
+ * Zone JS is required by default for Angular itself.
  */
 import 'zone.js/dist/zone';  // Included with Angular CLI.
-
-
-
-/***************************************************************************************************
- * APPLICATION IMPORTS
- */
-
-/**
- * Date, currency, decimal and percent pipes.
- * Needed for: All but Chrome, Firefox, Edge, IE11 and Safari 10
- */
-// import 'intl';  // Run `npm install --save intl`.
