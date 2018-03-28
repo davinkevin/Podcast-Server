b17223e4d59ec1cfd9c4807ef5cf7693b8a01d12
davinkevin <davin.kevin@gmail.com>
2018-03-28 05:32:21 +0200

    fix(noop): add some logic to noop downloader
    The default behavior cause the operation to be stuck in the download list. To avoid that, the NoOpDownloader will call the IDM at start to prevent stucking inside the downloading queue.

--- Backend/src/test/java/lan/dk/podcastserver/manager/downloader/NoOpDownloaderTest.java
+++ Backend/src/test/java/lan/dk/podcastserver/manager/downloader/NoOpDownloaderTest.java
@@ -1,8 +1,11 @@
 package lan.dk.podcastserver.manager.downloader;
 
+import lan.dk.podcastserver.entity.Item;
+import lan.dk.podcastserver.manager.ItemDownloadManager;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
 
 /**
  * Created by kevin on 16/03/2016 for Podcast Server
@@ -15,12 +18,10 @@ public class NoOpDownloaderTest {
         NoOpDownloader noOpDownloader = new NoOpDownloader();
 
         /* When */
-        noOpDownloader.startDownload();
         noOpDownloader.pauseDownload();
         noOpDownloader.restartDownload();
         noOpDownloader.stopDownload();
         noOpDownloader.finishDownload();
-        noOpDownloader.failDownload();
         noOpDownloader.run();
 
         /* Then */
@@ -30,5 +31,23 @@ public class NoOpDownloaderTest {
         assertThat(noOpDownloader.getItemUrl(null)).isNull();
         assertThat(noOpDownloader.compatibility(null)).isEqualTo(-1);
         assertThat(noOpDownloader.setItemDownloadManager(null)).isEqualTo(noOpDownloader);
+    }
+
+    @Test
+    public void should_remove_itself_from_idm_when_start() {
+        /* GIVEN */
+        ItemDownloadManager idm = mock(ItemDownloadManager.class);
+        DownloadingItem di = DownloadingItem.builder()
+                .item(Item.DEFAULT_ITEM)
+                .build();
+        NoOpDownloader noOpDownloader = new NoOpDownloader()
+                .setDownloadingItem(di)
+                .setItemDownloadManager(idm);
+
+        /* WHEN  */
+        noOpDownloader.run();
+
+        /* THEN  */
+        verify(idm, times(1)).removeACurrentDownload(Item.DEFAULT_ITEM);
     }
 }
