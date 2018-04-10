3f756e0815849bcbd9d188133c22834f227558fd
davinkevin <davin.kevin@gmail.com>
2018-04-10 05:37:19 +0200

    feat(front): serve both front so we can start playing with the new version right now
    Even if this front isn't finished yet, we can consult and see what it should be done with the highest priority

--- Backend/src/main/java/lan/dk/podcastserver/controller/HomeController.java
+++ Backend/src/main/java/lan/dk/podcastserver/controller/HomeController.java
@@ -8,7 +8,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
 
     @RequestMapping(value = {"", "items", "podcasts", "podcasts/**", "player", "download", "stats"})
-    public String home() {
+    public String v1() {
         return "index";
+    }
+
+    @RequestMapping(value = {"v2", "v2/search", "v2/items", "v2/podcasts", "v2/podcasts/**", "v2/player", "v2/download", "v2/stats"})
+    public String v2() {
+        return "v2/index";
     }
 }
