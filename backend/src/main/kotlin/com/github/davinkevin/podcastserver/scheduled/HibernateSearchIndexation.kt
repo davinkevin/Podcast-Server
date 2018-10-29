edb8fc9261d5dcf7f54316f8fe33af5ceaf201ad
davinkevin <davin.kevin@gmail.com>
2018-10-29 06:51:42 +0100

    refactor(kotlin): migration of the HibernateSearchIndexation to Kotlin

---
+++ backend/src/main/kotlin/com/github/davinkevin/podcastserver/scheduled/HibernateSearchIndexation.kt
@@ -0,0 +1,16 @@
+package com.github.davinkevin.podcastserver.scheduled
+
+import lan.dk.podcastserver.business.ItemBusiness
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Component
+
+/**
+ * Created by kevin on 22/08/2014.
+ */
+@Component
+class HibernateSearchIndexation(private val itemBusiness: ItemBusiness) {
+
+    @Scheduled(fixedDelay = 86400000)
+    fun refreshIndex() = itemBusiness.reindex()
+
+}
---
+++ backend/src/test/kotlin/com/github/davinkevin/podcastserver/scheduled/HibernateSearchIndexationTest.kt
@@ -0,0 +1,28 @@
+package com.github.davinkevin.podcastserver.scheduled
+
+import lan.dk.podcastserver.business.ItemBusiness
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.Mockito.times
+import org.mockito.Mockito.verify
+import org.mockito.junit.jupiter.MockitoExtension
+
+/**
+ * Created by kevin on 17/08/15 for Podcast Server
+ */
+@ExtendWith(MockitoExtension::class)
+class HibernateSearchIndexationTest {
+
+    @Mock lateinit var itemBusiness: ItemBusiness
+    @InjectMocks lateinit var hibernateSearchIndexation: HibernateSearchIndexation
+
+    @Test
+    fun should_refresh_index() {
+        /* When */
+        hibernateSearchIndexation.refreshIndex()
+        /* Then */
+        verify(itemBusiness, times(1)).reindex()
+    }
+}
