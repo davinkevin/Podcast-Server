217d253c4f3ef492c45d41bcb22e8685faa9209e
davinkevin <davin.kevin@gmail.com>
2018-10-30 07:38:21 +0100

    refactor(kotlin): migration of the UpdateScheduled to Kotlin

---
+++ backend/src/test/kotlin/com/github/davinkevin/podcastserver/scheduled/UpdateScheduledTest.kt
@@ -0,0 +1,32 @@
+package com.github.davinkevin.podcastserver.scheduled
+
+import com.github.davinkevin.podcastserver.scheduled.UpdateScheduled
+import lan.dk.podcastserver.business.update.UpdatePodcastBusiness
+import lan.dk.podcastserver.manager.ItemDownloadManager
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.Mockito.times
+import org.mockito.Mockito.verify
+import org.mockito.junit.jupiter.MockitoExtension
+
+/**
+ * Created by kevin on 17/08/15 for Podcast Server
+ */
+@ExtendWith(MockitoExtension::class)
+class UpdateScheduledTest {
+
+    @Mock lateinit var updatePodcastBusiness: UpdatePodcastBusiness
+    @Mock lateinit var IDM: ItemDownloadManager
+    @InjectMocks lateinit var updateScheduled: UpdateScheduled
+
+    @Test
+    fun should_update_and_download() {
+        /* When */
+        updateScheduled.updateAndDownloadPodcast()
+        /* Then */
+        verify(updatePodcastBusiness, times(1)).updatePodcast()
+        verify(IDM, times(1)).launchDownload()
+    }
+}
