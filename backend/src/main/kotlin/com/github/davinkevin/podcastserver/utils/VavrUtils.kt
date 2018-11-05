3cb2492ca46657207bb751ac990e675d97942d2a
davinkevin <davin.kevin@gmail.com>
2018-11-05 07:34:53 +0100

    refactor(utils): change implemnetation of utils in order to reduce warnings during compilation

--- backend/src/main/kotlin/com/github/davinkevin/podcastserver/utils/VavrUtils.kt
+++ backend/src/main/kotlin/com/github/davinkevin/podcastserver/utils/VavrUtils.kt
@@ -1,21 +1,14 @@
 package com.github.davinkevin.podcastserver.utils
 
-import arrow.core.None
-import arrow.core.Some
-import io.vavr.API
+import arrow.core.getOrElse
+import io.vavr.API.None
+import io.vavr.API.Option
 import io.vavr.collection.HashSet
 import io.vavr.collection.List
 
 /**
  * Created by kevin on 20/07/2018
  */
-
-fun <T> arrow.core.Option<T>.toVΛVΓ(): io.vavr.control.Option<T> {
-    return when(this) {
-        is Some -> API.Option(this.get())
-        is None -> API.None()
-    }
-}
-
-fun <T> Set<T>.toVΛVΓ(): io.vavr.collection.Set<T> = HashSet.ofAll(this)
+fun <T> arrow.core.Option<T>.toVΛVΓ(): io.vavr.control.Option<T> = this.map { Option(it) }.getOrElse { None() }
+fun <T> kotlin.collections.Set<T>.toVΛVΓ(): io.vavr.collection.Set<T> = HashSet.ofAll(this)
 fun <T> kotlin.collections.List<T>.toVΛVΓ(): io.vavr.collection.List<T> = List.ofAll(this)
