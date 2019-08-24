7895657c8c37a30cd3e2f9647b978bac782a77ff
davinkevin <davin.kevin@gmail.com>
2019-08-25 01:42:48 +0200

    fix(fs): better management of host name capture to handle redirect to fs component
    After investigation in K3S, `Host`, `X-Forwarded-Host`, `X-Forwarded-Proto`, `X-Forwarded-Port` are headers injected and usable from kotlin code of the backend.
    
    Closes #14

--- backend/src/main/kotlin/com/github/davinkevin/podcastserver/extension/ServerRequest/ServerRequestExtension.kt
+++ backend/src/main/kotlin/com/github/davinkevin/podcastserver/extension/ServerRequest/ServerRequestExtension.kt
@@ -1,5 +1,8 @@
 package com.github.davinkevin.podcastserver.extension.ServerRequest
 
+import arrow.core.orElse
+import arrow.syntax.collections.firstOption
+import org.slf4j.LoggerFactory
 import org.springframework.web.reactive.function.server.ServerRequest
 import java.net.URI
 
@@ -10,11 +13,21 @@ import java.net.URI
 val EMPTY_PORTS = setOf(80, 443)
 
 fun ServerRequest.extractHost(): URI {
-    val origin = this.headers().header("Origin").firstOrNull()
-    if (origin != null) {
-        return URI(origin)
-    }
-    val uri = this.uri()
-    val port = if(uri.port in EMPTY_PORTS) { "" } else { ":${uri.port}" }
-    return URI("${uri.scheme}://${uri.host}$port/")
+    val headers = this.headers()
+    val uri = uri()
+
+    val host = headers.header("Host").firstOrNull()
+            ?: headers.header("X-Forwarded-Host").firstOrNull()
+            ?: uri.host
+
+    val proto = headers.header("X-Forwarded-Proto").firstOrNull()
+            ?: uri.scheme
+
+    val port = headers.header("X-Forwarded-Port").map { it.toInt() }.firstOrNull()
+            ?: (if (uri.port == -1) null else uri.port)
+            ?: if (proto == "https") 443 else 80
+
+    val portAsString = if(port in EMPTY_PORTS) "" else ":$port"
+
+    return URI("$proto://$host$portAsString/")
 }
