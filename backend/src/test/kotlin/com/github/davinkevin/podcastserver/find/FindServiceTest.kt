d94b6038f1ddaa85b23bc2eada45457c4194fc0d
davinkevin <davin.kevin@gmail.com>
2019-11-02 12:51:34 +0100

    test(find): expands testing on find service

---
+++ backend/src/test/kotlin/com/github/davinkevin/podcastserver/find/FindServiceTest.kt
@@ -0,0 +1,64 @@
+package com.github.davinkevin.podcastserver.find
+
+import com.github.davinkevin.podcastserver.manager.worker.Finder
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.whenever
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Import
+import org.springframework.test.context.junit.jupiter.SpringExtension
+import reactor.kotlin.core.publisher.toMono
+import reactor.test.StepVerifier
+import java.net.URI
+
+@ExtendWith(SpringExtension::class)
+class FindServiceTest(
+        @Autowired private val service: FindService,
+        @Autowired private val firstFinder: Finder,
+        @Autowired private val secondFinder: Finder
+) {
+
+    @Test
+    fun `should find most compatible finder and delegate to it the find operation`() {
+        /* Given */
+        val url = "https://foo.bar.com"
+        val p = FindPodcastInformation(title = "", url = URI(url), type = "first", cover = null, description = "")
+        whenever(firstFinder.compatibility(url)).thenReturn(2)
+        whenever(firstFinder.findInformation(url)).thenReturn(p.toMono())
+        whenever(secondFinder.compatibility(url)).thenReturn(3)
+        /* When */
+        StepVerifier.create(service.find(URI(url)))
+                /* Then */
+                .expectSubscription()
+                .expectNext(p)
+                .verifyComplete()
+    }
+
+    @Test
+    fun `should fallback on default response if the selected finder crash during find operation`() {
+        /* Given */
+        val url = "https://foo.bar.com"
+        whenever(firstFinder.compatibility(url)).thenReturn(2)
+        whenever(firstFinder.findInformation(url)).thenReturn(RuntimeException("error !").toMono())
+        whenever(secondFinder.compatibility(url)).thenReturn(3)
+        /* When */
+        StepVerifier.create(service.find(URI(url)))
+                /* Then */
+                .expectSubscription()
+                .expectNext(FindPodcastInformation(title = "", url = URI(url), type = "RSS", cover = null, description = ""))
+                .verifyComplete()
+    }
+
+    @TestConfiguration
+    @Import(FindService::class)
+    class LocalTestConfiguration {
+        @Bean fun firstFinder() = mock<Finder>()
+        @Bean fun secondFinder() = mock<Finder>()
+    }
+}
