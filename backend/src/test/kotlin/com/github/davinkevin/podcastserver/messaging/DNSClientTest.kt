66351d2956bf0dab931e877d89a4828b98e029ad
davinkevin <davin.kevin@gmail.com>
2020-05-04 07:53:22 +0200

    fix(dns): provide a test running on any runner for test of dns client

--- backend/src/test/kotlin/com/github/davinkevin/podcastserver/messaging/DNSClientTest.kt
+++ backend/src/test/kotlin/com/github/davinkevin/podcastserver/messaging/DNSClientTest.kt
@@ -1,11 +1,7 @@
 package com.github.davinkevin.podcastserver.messaging
 
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.condition.EnabledOnOs
-import org.junit.jupiter.api.condition.OS
 import reactor.test.StepVerifier
 
 /**
@@ -13,49 +9,21 @@ import reactor.test.StepVerifier
  */
 class DNSClientTest {
 
-    @Nested
-    @DisplayName("OnLinux")
-    @EnabledOnOs(OS.LINUX)
-    inner class OnLinux {
-
-        @Test
-        fun `should fetch all by name`() {
-            /* Given */
-            val dns = DNSClient()
-            /* When */
-            StepVerifier.create(dns.allByName("localhost"))
-                    /* Then */
-                    .expectSubscription()
-                    .assertNext {
-                        assertThat(it.hostAddress).isEqualTo("127.0.0.1")
-                    }
-                    .verifyComplete()
-        }
-
+    @Test
+    fun `should fetch all by name`() {
+        /* Given */
+        val dns = DNSClient()
+        /* When */
+        StepVerifier.create(dns
+                .allByName("localhost")
+                .map { it.hostAddress }
+                .collectList()
+        )
+                /* Then */
+                .expectSubscription()
+                .assertNext {
+                    assertThat(it).contains("127.0.0.1")
+                }
+                .verifyComplete()
     }
-
-    @Nested
-    @DisplayName("OnMacOS")
-    @EnabledOnOs(OS.MAC)
-    inner class OnMacOS {
-
-        @Test
-        fun `should fetch all by name`() {
-            /* Given */
-            val dns = DNSClient()
-            /* When */
-            StepVerifier.create(dns.allByName("localhost"))
-                    /* Then */
-                    .expectSubscription()
-                    .assertNext {
-                        assertThat(it.hostAddress).isEqualTo("127.0.0.1")
-                    }
-                    .assertNext {
-                        assertThat(it.hostAddress).isEqualTo("0:0:0:0:0:0:0:1")
-                    }
-                    .verifyComplete()
-        }
-
-    }
-
 }
