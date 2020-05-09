b08d3c46df6d9ca2f3c532b3f5ed3bbe9d3df127
davinkevin <davin.kevin@gmail.com>
2020-05-09 11:14:19 +0200

    test(): expands test on configuration
    Include test for clock configuration

---
+++ backend/src/test/kotlin/com/github/davinkevin/podcastserver/config/ClockConfigTest.kt
@@ -0,0 +1,32 @@
+package com.github.davinkevin.podcastserver.config
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Import
+import org.springframework.test.context.junit.jupiter.SpringExtension
+import java.time.Clock
+import java.time.ZonedDateTime
+
+/**
+ * Created by kevin on 09/05/2020
+ */
+@ExtendWith(SpringExtension::class)
+@Import(ClockConfig::class)
+class ClockConfigTest(
+        @Autowired private val clock: Clock
+) {
+
+    @Test
+    fun `should provide clock sync on system`() {
+        /* Given */
+        val now = ZonedDateTime.now()
+        /* When */
+        val nowOnClock = ZonedDateTime.now(clock)
+        /* Then */
+        assertThat(nowOnClock).isEqualToIgnoringNanos(now)
+    }
+
+}
