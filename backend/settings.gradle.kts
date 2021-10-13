06c20f9b4cdd2ad68399b9aab18850ab69bf8afa
Kevin Davin <davin.kevin@gmail.com>
2021-10-13 17:42:19 +0200

    chore(backend): optimize build process using Gradle Enterprise
    The modification leverage cache available in Gradle Enterprise to improve developer productivity.
    Some changes have to be made, removing some side effect from build pipeline. So, flyway has been used in two
    dedicated tasks, one for JOOQ code creation and another for init database setup.
    
    Link to #132

--- backend/settings.gradle.kts
+++ backend/settings.gradle.kts
@@ -1,1 +1,34 @@
 rootProject.name = "backend"
+
+plugins {
+    id("com.gradle.enterprise") version("3.7")
+    id("com.gradle.common-custom-user-data-gradle-plugin") version "1.4.2"
+}
+
+val isCI = System.getenv("CI") == "true"
+
+gradleEnterprise {
+    server = "https://ge.3.227.211.215.nip.io"
+    allowUntrustedServer = true
+
+    buildScan {
+        publishAlways()
+        allowUntrustedServer = true
+
+        capture {
+            isTaskInputFiles = true
+            isUploadInBackground = !isCI
+        }
+    }
+
+}
+
+buildCache {
+    local { isEnabled = !isCI }
+    remote<HttpBuildCache> {
+        url = uri("https://ge.3.227.211.215.nip.io/cache/")
+        isPush = isCI
+        isEnabled = true
+        isAllowUntrustedServer = true
+    }
+}
