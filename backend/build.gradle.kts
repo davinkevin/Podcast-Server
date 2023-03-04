b48e333a893591aeaed2cd760e9bfd064e33d88d
davinkevin <davin.kevin@gmail.com>
2023-03-04 14:25:42 +0100

    feat(backend): upgrade to spring boot 3.0.4
    Closes #192

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -5,13 +5,13 @@ import com.gitlab.davinkevin.podcastserver.database.*
 import com.gitlab.davinkevin.podcastserver.dockerimages.*
 
 plugins {
-	id("org.springframework.boot") version "3.0.3"
+	id("org.springframework.boot") version "3.0.4"
 	id("io.spring.dependency-management") version "1.1.0"
 
 	id("com.gorylenko.gradle-git-properties") version "2.4.1"
 	id("com.google.cloud.tools.jib") version "3.3.1"
 	id("org.jetbrains.kotlinx.kover") version "0.6.1"
-	id("org.graalvm.buildtools.native") version "0.9.19"
+	id("org.graalvm.buildtools.native") version "0.9.20"
 
 	kotlin("jvm") version "1.7.22"
 	kotlin("plugin.spring") version "1.7.22"
