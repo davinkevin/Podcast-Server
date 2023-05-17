23d6f2d01b9e03275289c96ff940d4b37364868d
davinkevin <davin.kevin@gmail.com>
2023-05-17 18:46:56 +0200

    feat(frontend): move to Static Web Server instead of nginx for frontend hosting
    Closes #194

--- ui/compile.sh
+++ ui/compile.sh
@@ -5,11 +5,12 @@ UI_FOLDER=ui
 rm -rf ${UI_FOLDER}/target
 mkdir -p ${UI_FOLDER}/target/docker
 
-cp -r ${UI_FOLDER}/src/docker/Dockerfile \
-    ${UI_FOLDER}/src/conf/default.conf \
-    ${UI_FOLDER}/target/docker
+echo "Injection of SWS configuration"
+cp -r ${UI_FOLDER}/src/docker/* ${UI_FOLDER}/target/docker/
 
+echo "Injection of ui-v1 files"
 cp -r frontend-angularjs/target/dist ${UI_FOLDER}/target/docker/podcast-server
 
+echo "Injection of ui-v2 files"
 mkdir -p ${UI_FOLDER}/target/docker/podcast-server/v2/
 cp frontend-angular/dist/* ${UI_FOLDER}/target/docker/podcast-server/v2/
