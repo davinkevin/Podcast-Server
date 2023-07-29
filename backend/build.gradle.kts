54dcf2e85b277fc4d29736f73a63b378db583f22
davinkevin <davin.kevin@gmail.com>
2023-07-29 11:43:13 +0200

    feat(backend): upgrade spring boot to v3.1.2 and related libraries
    Closes #207

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -6,13 +6,13 @@ import com.gitlab.davinkevin.podcastserver.dockerimages.*
 import com.gradle.enterprise.gradleplugin.testretry.retry
 
 plugins {
-	id("org.springframework.boot") version "3.1.1"
-	id("io.spring.dependency-management") version "1.1.0"
+	id("org.springframework.boot") version "3.1.2"
+	id("io.spring.dependency-management") version "1.1.2"
 
 	id("com.gorylenko.gradle-git-properties") version "2.4.1"
 	id("com.google.cloud.tools.jib") version "3.3.2"
 	id("org.jetbrains.kotlinx.kover") version "0.7.0"
-	id("org.graalvm.buildtools.native") version "0.9.22"
+	id("org.graalvm.buildtools.native") version "0.9.23"
 
 	kotlin("jvm") version "1.8.22"
 	kotlin("plugin.spring") version "1.8.22"
