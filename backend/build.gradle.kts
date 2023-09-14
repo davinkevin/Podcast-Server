fbd54cd1e7bc0bc6259368ff0caf738da89561a0
Davin Kevin <davin.kevin@gmail.com>
2023-09-14 21:13:28 +0200

    chore(build): upgrade to kover 0.7.3

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 
 	id("com.gorylenko.gradle-git-properties") version "2.4.1"
 	id("com.google.cloud.tools.jib") version "3.3.2"
-	id("org.jetbrains.kotlinx.kover") version "0.7.0"
+	id("org.jetbrains.kotlinx.kover") version "0.7.3"
 	id("org.graalvm.buildtools.native") version "0.9.23"
 
 	kotlin("jvm") version "1.8.22"
