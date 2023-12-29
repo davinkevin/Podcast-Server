4d0818ac9a8f94e6619bc8d5869703b0623d4e38
Davin Kevin <davin.kevin@gmail.com>
2023-12-29 17:32:57 +0100

    feat(backend): add support for virtualThread for backend
    Closes #225

--- backend/src/main/kotlin/com/github/davinkevin/podcastserver/download/DownloadConfig.kt
+++ backend/src/main/kotlin/com/github/davinkevin/podcastserver/download/DownloadConfig.kt
@@ -1,12 +1,13 @@
 package com.github.davinkevin.podcastserver.download
 
 import com.github.davinkevin.podcastserver.service.properties.PodcastServerParameters
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Import
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor
 import org.springframework.web.reactive.function.server.router
-import kotlin.concurrent.thread
 
 /**
  * Created by kevin on 17/09/2019
@@ -50,8 +51,18 @@ class DownloadRouterConfig {
         DownloadRepository::class
 )
 class DownloadConfig {
+
+    private val log = LoggerFactory.getLogger(DownloadConfig::class.java)
+
     @Bean
-    fun downloadExecutor(parameters: PodcastServerParameters) = ThreadPoolTaskExecutor().apply {
+    fun downloadExecutor(
+        parameters: PodcastServerParameters,
+        @Value("\${spring.threads.virtual.enabled:false}") isVirtualThreadEnabled: Boolean
+    ) = ThreadPoolTaskExecutor().apply {
+        if (isVirtualThreadEnabled) {
+            log.info("Downloader thread pool is based on virtual threads")
+            newThread(Thread::ofVirtual)
+        }
         corePoolSize = parameters.concurrentDownload
         setThreadNamePrefix("Downloader-")
         initialize()
