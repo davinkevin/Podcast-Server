61639b4d82ccb602ffb1e0a903a2d3b9d632585b
Davin Kevin <davin.kevin@gmail.com>
2024-04-20 07:37:25 +0200

    refactor(deps): introduce tomcat server as backend

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -47,7 +47,10 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter")
-	implementation("org.springframework.boot:spring-boot-starter-webflux")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-webflux") {
+		exclude("org.springframework.boot:spring-boot-starter-reactor-netty")
+	}
 	implementation("org.springframework.boot:spring-boot-starter-jooq")
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("io.micrometer:micrometer-registry-prometheus")
